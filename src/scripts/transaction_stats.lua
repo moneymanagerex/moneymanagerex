ca6b2f71cd5968462ab72a6fa069bddec07c7dfe
vomikan <vomikan@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-06-17 20:17:58 +0000

    json emulation added
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@4114 7f5d68d0-c712-0410-839f-9b116a576cc5

--- src/scripts/transaction_stats.lua
+++ src/scripts/transaction_stats.lua
@@ -1,17 +1,17 @@
-local function add_header(m_stat)
+local function add_header(header)
     mmHTMLTableHeaderCell(_("Month/Year"))
-    for y,v in pairs(m_stat[1]) do
-        mmHTMLTableHeaderCell(y) 
+    for y in pairs(header) do
+        mmHTMLTableHeaderCell(header[y]) 
     end
 end
 local function get_stats(m_stat)
     local total = 0
     mmHTMLStartTable("50%")
-    add_header(m_stat)
-    for m, data in pairs(m_stat) do
+    add_header(m_stat.header)
+    for m, data in pairs(m_stat.data) do
         mmHTMLStartTableRow()
         mmHTMLTableCellMonth(m)
-        for y, val in pairs(data) do
+        for i, val in pairs(data) do
             total = total + val
             mmHTMLTableCellInteger(val)
         end
@@ -24,20 +24,35 @@ end
 --Main function
 -----------------------------------------------------------
 local function main(m_stat)
-    local json_text = '{ "title": "Transaction Statistics" }'
-    local value = cjson.decode(json_text)
 
-    mmHTMLReportHeader(value.title)
+    mmHTMLReportHeader(m_stat.title)
     mmHTMLhr()
     total = get_stats(m_stat)
     mmHTMLhr()
     mmHTMLaddText(_("Total Transactions: ") .. total)
 end
 
---test table
-local m_stat = {}
-local y_stat = {[2014]=1,[2013]=2,[2012]=3,[2011]=4,[2010]=5,[2009]=6,[2008]=7,[2007]=8,[2006]=9,[2005]=10}
-table.sort(y_stat)
-for m = 1, 12, 1 do m_stat[m] = (y_stat) end
---test end
+--test
+local json_data = [[
+{
+	"title": "Transaction Statistics",
+	"header": ["2014","2013","2012","2011","2010","2009","2008","2007","2006","2005"],
+	"data": {
+		"1": ["1","2","3","4","5","6","7","8","9","10"],
+		"2": ["11","12","13","14","15","16","17","18","19","20"],
+		"3": ["21","22","23","24","25","26","27","28","29","30"],
+		"4": ["31","32","33","34","35","36","37","38","39","40"],
+		"5": ["41","42","43","44","45","46","47","48","49","50"],
+		"6": ["51","52","53","54","55","56","57","58","59","60"],
+		"7": ["71","72","73","74","75","76","77","78","79","80"],
+		"8": ["81","82","83","84","85","86","87","88","89","90"],
+		"9": ["91","92","93","94","95","96","97","98","99","100"],
+		"10": ["101","102","103","104","105","106","107","108","109","110"],
+		"11": ["111","112","113","114","115","116","117","118","119","120"],
+		"12": ["121","122","123","124","125","126","127","128","129","130"]
+	}
+}
+]]
+local m_stat = cjson.decode(json_data)
 main(m_stat)
+--test end
