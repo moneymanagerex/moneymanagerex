ec2a82e5892614b7b6ede822a164dec622f09211
guanlisheng <guanlisheng@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-07-07 03:07:45 +0000

    Model_Stock.h sketch
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@4302 7f5d68d0-c712-0410-839f-9b116a576cc5

---
+++ src/model/Model_Stock.h
@@ -0,0 +1,45 @@
+// -*- C++ -*-
+//=============================================================================
+/**
+ *      Copyright (c) 2013 Guan Lisheng (guanlisheng@gmail.com)
+ *
+ *      @file
+ *
+ *      @author [sqlite2cpp.py]
+ *
+ *      @brief
+ *
+ *      Revision History:
+ *          AUTO GENERATED at 2013-07-03 22:55:49.491495.
+ *          DO NOT EDIT!
+ */
+//=============================================================================
+
+#ifndef MODEL_STOCK_H
+#define MODEL_STOCK_H
+
+#include "Model.h"
+#include "db/DB_Table_Stock_V1.h"
+#include "Model_Currency.h" // detect base currency
+
+class Model_Stock : public Model, public DB_Table_STOCK_V1
+{
+    using DB_Table_STOCK_V1::all;
+public:
+    Model_Stock(): Model(), DB_Table_STOCK_V1() {};
+    ~Model_Stock() {};
+
+public:
+    static Model_Stock& instance()
+    {
+        return Singleton<Model_Stock>::instance();
+    }
+
+public:
+    Data_Set all(COLUMN col = COLUMN(0), bool asc = true)
+    {
+        return all(db_, col, asc);
+    }
+};
+
+#endif // 
