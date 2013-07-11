ebea01a4420ec7efa6ba1190447ffcb8bcdbe529
vomikan <vomikan@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-07-11 12:45:28 +0000

    Empty classes for export
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@4350 7f5d68d0-c712-0410-839f-9b116a576cc5

---
+++ src/import_export/export.cpp
@@ -0,0 +1,19 @@
+/*******************************************************
+  Copyright (C) 2013 Nikolay
+ ********************************************************/
+
+#include "export.h"
+
+mmExportTransaction::mmExportTransaction(mmCoreDB* core, mmBankTransaction* pBankTransaction)
+    : mmPrintableBase(core)
+    , pBankTransaction_(pBankTransaction)
+{}
+
+mmExportTransaction::~mmExportTransaction()
+{};
+
+wxString mmExportTransaction::getTransactionQIF()
+{
+    core_->currencyList_.LoadBaseCurrencySettings();
+    return "test";
+}
---
+++ src/import_export/export.h
@@ -0,0 +1,34 @@
+/*******************************************************
+  Copyright (C) 2013 Nikolay
+ ********************************************************/
+
+#ifndef _MM_EX_EXPORT_H_
+#define _MM_EX_EXPORT_H_
+
+#include "../reports/reportbase.h"
+
+class mmExportTransaction : public mmPrintableBase
+{
+
+public:
+    ~mmExportTransaction();
+    mmExportTransaction(mmCoreDB* core
+        , mmBankTransaction* pBankTransaction);
+
+    wxString getTransactionQIF();
+
+protected:
+    wxString data_;
+    mmBankTransaction* pBankTransaction_;
+    mmCoreDB* core_;
+};
+
+class mmExportTransactionQIF: public mmExportTransaction
+{
+public:
+    mmExportTransactionQIF(mmCoreDB* core, mmBankTransaction* pBankTransaction) 
+        : mmExportTransaction(core, pBankTransaction)
+    {}
+};
+
+#endif 
