50f9997a6f85b71427bbd3e7063f22350b46ad6b
siena123 <siena123@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-09-21 18:44:21 +0000

    Setup cppunit framework. Add one unit test.
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@4536 7f5d68d0-c712-0410-839f-9b116a576cc5

---
+++ src/tests/currency_test.cpp
@@ -0,0 +1,117 @@
+/*******************************************************
+Copyright (C) 2013 James Higley
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Placeuite 330, Boston, MA  02111-1307  USA
+ ********************************************************/
+
+#include "currency_test.h"
+#include "mmCurrency.h"
+#include "mmCurrencyFormatter.h"
+#include <wx/string.h>
+#include <cppunit/config/SourcePrefix.h>
+
+// Registers the fixture into the 'registry'
+CPPUNIT_TEST_SUITE_REGISTRATION( CurrencyTest );
+
+CurrencyTest::CurrencyTest()
+{
+}
+
+
+CurrencyTest::~CurrencyTest()
+{
+}
+
+void CurrencyTest::setUp()
+{
+	locale_.Init(wxLANGUAGE_ENGLISH);
+
+	mmCurrency* currency = new mmCurrency("USD", "United States dollar", L"$", L"", L"", L"", 100, 1);
+	CurrencyFormatter::instance().loadSettings(*currency);
+	delete currency;
+}
+
+void CurrencyTest::tearDown()
+{
+}
+
+void CurrencyTest::formatDoubleToCurrency()
+{
+	double value1 = 1234.1415;
+	double value2 = 0.0099;
+
+	wxString s = CurrencyFormatter::float2Money(0);
+	CPPUNIT_ASSERT(s == wxT("$0.00"));
+
+	s = CurrencyFormatter::float2Money(-0);
+	CPPUNIT_ASSERT(s == wxT("$0.00"));
+
+	s = CurrencyFormatter::float2Money(1.0);
+	CPPUNIT_ASSERT(s == wxT("$1.00"));
+
+	s = CurrencyFormatter::float2Money(-12.3);
+	CPPUNIT_ASSERT(s == wxT("$-12.30"));
+
+	s = CurrencyFormatter::float2Money(123.4);
+	CPPUNIT_ASSERT(s == wxT("$123.40"));
+
+	s = CurrencyFormatter::float2Money(-1000.0);
+	CPPUNIT_ASSERT(s == wxT("$-1,000.00"));
+
+	s = CurrencyFormatter::float2Money(10000.0);
+	CPPUNIT_ASSERT(s == wxT("$10,000.00"));
+
+	s = CurrencyFormatter::float2Money(-100000.0);
+	CPPUNIT_ASSERT(s == wxT("$-100,000.00"));
+
+	s = CurrencyFormatter::float2Money(1000000.0);
+	CPPUNIT_ASSERT(s == wxT("$1,000,000.00"));
+
+	s = CurrencyFormatter::float2Money(-10000000.0);
+	CPPUNIT_ASSERT(s == wxT("$-10,000,000.00"));
+
+	s = CurrencyFormatter::float2Money(100000000.0);
+	CPPUNIT_ASSERT(s == wxT("$100,000,000.00"));
+
+	s = CurrencyFormatter::float2Money(1000000000.0);
+	CPPUNIT_ASSERT(s == wxT("$1,000,000,000.00"));
+
+	s = CurrencyFormatter::float2Money(-10000000000.0);
+	CPPUNIT_ASSERT(s == wxT("$-10,000,000,000.00"));
+
+	s = CurrencyFormatter::float2Money(100000000000.0);
+	CPPUNIT_ASSERT(s == wxT("$100,000,000,000.00"));
+
+	s = CurrencyFormatter::float2Money(-1234567890123.45);
+	CPPUNIT_ASSERT(s == wxT("$-1,234,567,890,123.45"));
+
+	s = CurrencyFormatter::float2Money(value1);
+	CPPUNIT_ASSERT(s == wxT("$1,234.14"));
+
+	s = CurrencyFormatter::float2Money(-value1);
+	CPPUNIT_ASSERT(s == wxT("$-1,234.14"));
+
+	s = CurrencyFormatter::float2Money(value2);
+	CPPUNIT_ASSERT(s == wxT("$0.01"));
+
+	s = CurrencyFormatter::float2Money(-value2);
+	CPPUNIT_ASSERT(s == wxT("$-0.01"));
+
+	s = CurrencyFormatter::float2Money(0.004);
+	CPPUNIT_ASSERT(s == wxT("$0.00"));
+
+	s = CurrencyFormatter::float2Money(0.005);
+	CPPUNIT_ASSERT(s == wxT("$0.01"));
+}
--- src/tests/mmex_tests.cpp
+++ src/tests/mmex_tests.cpp
@@ -18,6 +18,12 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 #include "../platfdep.h" // GetAppName
 #include <wx/app.h>
+#include <cppunit/BriefTestProgressListener.h>
+#include <cppunit/CompilerOutputter.h>
+#include <cppunit/extensions/TestFactoryRegistry.h>
+#include <cppunit/TestResult.h>
+#include <cppunit/TestResultCollector.h>
+#include <cppunit/TestRunner.h>
 //----------------------------------------------------------------------------
 
 /*
@@ -31,6 +37,36 @@ int main(int /*argc*/, char const* /*argv*/[])
 
     wxApp::GetInstance()->SetAppName(mmex::GetAppName());
 
-    return 0;
+	// Create the event manager and test controller
+	CPPUNIT_NS::TestResult controller;
+
+	// Add a listener that colllects test result
+	CPPUNIT_NS::TestResultCollector result;
+	controller.addListener( &result );        
+
+	// Add a listener that print dots as test run.
+	CPPUNIT_NS::BriefTestProgressListener progress;
+	controller.addListener( &progress );      
+
+	// Add the top suite to the test runner
+	CPPUNIT_NS::TestRunner runner;
+	runner.addTest( CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest() );
+	try
+	{
+		runner.run( controller );
+
+		// Print test in a compiler compatible format.
+		CPPUNIT_NS::CompilerOutputter outputter( &result, CPPUNIT_NS::stdCOut() );
+		outputter.write(); 
+	}
+	catch ( std::invalid_argument &e )  // Test path not resolved
+	{
+		CPPUNIT_NS::stdCOut()	<<  "\n"  
+								<<  "ERROR: "  <<  e.what()
+								<< "\n";
+		return 0;
+	}
+
+	return result.wasSuccessful() ? 0 : 1;
 }
 //----------------------------------------------------------------------------
