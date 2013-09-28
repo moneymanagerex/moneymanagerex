5350b7f6ba15c1d52c4b9511a787f52758c5b4b2
guanlisheng <guanlisheng@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-09-28 12:18:53 +0000

    Model_Splittransaction.h added
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@4613 7f5d68d0-c712-0410-839f-9b116a576cc5

---
+++ src/model/Model_Splittransaction.h
@@ -0,0 +1,71 @@
+// -*- C++ -*-
+//=============================================================================
+/**
+ *      Copyright (c) 2013 Guan Lisheng (guanlisheng@gmail.com)
+ *
+ *      @file
+ *
+ *      @author [sqlite2cpp.py]
+ *
+ *      @brief
+ *
+ *      Revision History:
+ *          AUTO GENERATED at 2013-07-03 22:55:49.491495.
+ *          DO NOT EDIT!
+ */
+//=============================================================================
+
+#ifndef MODEL_SPLITTRANSACTIONS_H
+#define MODEL_SPLITTRANSACTIONS_H
+
+#include "Model.h"
+#include "db/DB_Table_Splittransactions_v1.h"
+
+class Model_Splittransaction : public Model, public DB_Table_SPLITTRANSACTIONS_V1
+{
+    using DB_Table_SPLITTRANSACTIONS_V1::all;
+    using DB_Table_SPLITTRANSACTIONS_V1::find;
+    using DB_Table_SPLITTRANSACTIONS_V1::get;
+public:
+    Model_Splittransaction(): Model(), DB_Table_SPLITTRANSACTIONS_V1() 
+    {
+    };
+    ~Model_Splittransaction() {};
+
+public:
+    wxArrayString types_;
+
+public:
+    static Model_Splittransaction& instance()
+    {
+        return Singleton<Model_Splittransaction>::instance();
+    }
+    static Model_Splittransaction& instance(wxSQLite3Database* db)
+    {
+        Model_Splittransaction& ins = Singleton<Model_Splittransaction>::instance();
+        ins.db_ = db;
+        return ins;
+    }
+public:
+    Data_Set all(COLUMN col = COLUMN(0), bool asc = true)
+    {
+        this->ensure(this->db_);
+        return all(db_, col, asc);
+    }
+    template<class V>
+    Data_Set find(COLUMN col, const V& v)
+    {
+        return find(db_, col, v);
+    }
+    Data* get(int id)
+    {
+        return this->get(id, this->db_);
+    }
+    int save(Data* r)
+    {
+        r->save(this->db_);
+        return r->id();
+    }
+};
+
+#endif // 
