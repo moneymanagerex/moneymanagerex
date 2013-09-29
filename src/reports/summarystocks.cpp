723a40e110827f42310c8c0b42c6e71287b8cf21
siena123 <siena123@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-09-29 01:43:01 +0000

    report updated to sort
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@4624 7f5d68d0-c712-0410-839f-9b116a576cc5

--- src/reports/summarystocks.cpp
+++ src/reports/summarystocks.cpp
@@ -32,10 +32,24 @@
 #include "util.h"
 #endif
 
+#include <algorithm>
+
+#define STOCK_SORT_BY_NAME			1
+#define STOCK_SORT_BY_SYMBOL		2
+#define STOCK_SORT_BY_DATE			3
+#define STOCK_SORT_BY_QTY			4
+#define STOCK_SORT_BY_PUR_PRICE		5
+#define STOCK_SORT_BY_CUR_PRICE		6
+#define STOCK_SORT_BY_COMMISSION	7
+#define STOCK_SORT_BY_GAIN_LOSS		8
+#define STOCK_SORT_BY_VALUE			9
 
 mmReportSummaryStocks::mmReportSummaryStocks(mmCoreDB* core)
 : mmPrintableBase(core)
-{}
+{
+	// set initial sort column
+	sortColumn_ = STOCK_SORT_BY_NAME;
+}
 
 static const char SELECT_ROW_SYMBOL_FROM_STOCK_V1[] =
     "SELECT "
@@ -53,11 +67,10 @@ static const char SELECT_ROW_SYMBOL_FROM_STOCK_V1[] =
 
 wxString mmReportSummaryStocks::getHTMLText()
 {
-    mmHTMLBuilder hb;
-    hb.init();
-
-    hb.addHeader(2, _("Summary of Stocks"));
-    hb.addDateNow();
+	// structure for sorting of data
+    struct data_holder {wxString name; wxString symbol; wxString date; double qty; double purchase; double current; double commission; double gainloss; double value;} line;
+	struct account_holder {wxString name; std::vector<data_holder> data; double gainloss; double total;} account;
+    std::vector<account_holder> stocks;
 
     double gain_loss_sum_total = 0.0;
     double stockBalance = 0.0;
@@ -72,18 +85,17 @@ wxString mmReportSummaryStocks::getHTMLText()
     account_type.Add(ACCOUNT_TYPE_STOCK);
     const wxArrayInt accounts_id = core_->accountList_.getAccountsID(account_type);
 
-    hb.startTable("95%");
     for (size_t i = 0; i < accounts_id.Count(); ++i)
     {
         const int accountID  = accounts_id[i];
         if ( mmAccount::MMEX_Open != core_->accountList_.GetAccountSharedPtr(accountID)->status_) break;
-        double gain_loss_sum = 0.0;
-        wxString heldAt_ = core_->accountList_.GetAccountName(accountID);
 
-        hb.addTotalRow("", 9, "");
-        hb.addTotalRow(heldAt_, 9, "");
+		account.name = core_->accountList_.GetAccountName(accountID);
+		account.gainloss = 0.0;
+        double invested = 0;
+		account.total = mmDBWrapper::getStockInvestmentBalance(core_->db_.get(), accountID, invested);
+		account.data.clear();
 
-        display_header(hb);
         wxSQLite3Statement st = core_->db_->PrepareStatement(SELECT_ROW_SYMBOL_FROM_STOCK_V1);
         st.Bind(1, accountID);
         wxSQLite3ResultSet q2 = st.ExecuteQuery();
@@ -108,32 +120,145 @@ wxString mmReportSummaryStocks::getHTMLText()
             th.gainLoss_        = th.value_ - ((th.numShares_ * th.purchasePrice_) + commission);
             double base_conv_rate = core_->accountList_.getAccountBaseCurrencyConvRate(accountID);
             stockBalance += base_conv_rate * th.value_;
-            gain_loss_sum += th.gainLoss_;
+            account.gainloss += th.gainLoss_;
             gain_loss_sum_total += th.gainLoss_ * base_conv_rate;
 
-            hb.startTableRow();
-            hb.addTableCell(th.shareName_, false, true);
-            hb.addTableCell(th.stockSymbol_);
-            hb.addTableCell(dt);
-			hb.addMoneyCell(th.numShares_);
-            hb.addMoneyCell(th.purchasePrice_);
-            hb.addMoneyCell(th.currentPrice_);
-			hb.addMoneyCell(commission);
-			hb.addMoneyCell(th.gainLoss_);
-            hb.addMoneyCell(th.value_);
-            hb.endTableRow();
-
+			line.name = th.shareName_;
+			line.symbol = th.stockSymbol_;
+			line.date = dt;
+			line.qty = th.numShares_;
+			line.purchase = th.purchasePrice_;
+			line.current = th.currentPrice_;
+			line.commission = commission;
+			line.gainloss = th.gainLoss_;
+			line.value = th.value_;
+			account.data.push_back(line);
         }
         q2.Finalize();
 
-        double invested = 0;
-        double stockBalance = mmDBWrapper::getStockInvestmentBalance(core_->db_.get(), accountID, invested);
-
-        hb.addRowSeparator(9);
-        hb.addTotalRow(_("Total:"), 8, gain_loss_sum);
-		hb.addMoneyCell(stockBalance);
+		stocks.push_back(account);
     }
 #endif
+
+    for (auto& acct : stocks)
+	{
+		switch (sortColumn_)
+		{
+		case STOCK_SORT_BY_SYMBOL:
+			std::stable_sort(acct.data.begin(), acct.data.end()
+					, [] (const data_holder& x, const data_holder& y)
+					{
+						return x.symbol < y.symbol;
+					}
+			);
+			break;
+		case STOCK_SORT_BY_DATE:
+			std::stable_sort(acct.data.begin(), acct.data.end()
+					, [] (const data_holder& x, const data_holder& y)
+					{
+						if (x.date != y.date) return x.date < y.date;
+						else return x.name < y.name;
+					}
+			);
+			break;
+		case STOCK_SORT_BY_QTY:
+			std::stable_sort(acct.data.begin(), acct.data.end()
+					, [] (const data_holder& x, const data_holder& y)
+					{
+						if (x.qty != y.qty) return x.qty < y.qty;
+						else return x.name < y.name;
+					}
+			);
+			break;
+		case STOCK_SORT_BY_PUR_PRICE:
+			std::stable_sort(acct.data.begin(), acct.data.end()
+					, [] (const data_holder& x, const data_holder& y)
+					{
+						if (x.purchase != y.purchase) return x.purchase < y.purchase;
+						else return x.name < y.name;
+					}
+			);
+			break;
+		case STOCK_SORT_BY_CUR_PRICE:
+			std::stable_sort(acct.data.begin(), acct.data.end()
+					, [] (const data_holder& x, const data_holder& y)
+					{
+						if (x.current != y.current) return x.current < y.current;
+						else return x.name < y.name;
+					}
+			);
+			break;
+		case STOCK_SORT_BY_COMMISSION:
+			std::stable_sort(acct.data.begin(), acct.data.end()
+					, [] (const data_holder& x, const data_holder& y)
+					{
+						if (x.commission != y.commission) return x.commission < y.commission;
+						else return x.name < y.name;
+					}
+			);
+			break;
+		case STOCK_SORT_BY_GAIN_LOSS:
+			std::stable_sort(acct.data.begin(), acct.data.end()
+					, [] (const data_holder& x, const data_holder& y)
+					{
+						if (x.gainloss != y.gainloss) return x.gainloss < y.gainloss;
+						else return x.name < y.name;
+					}
+			);
+			break;
+		case STOCK_SORT_BY_VALUE:
+			std::stable_sort(acct.data.begin(), acct.data.end()
+					, [] (const data_holder& x, const data_holder& y)
+					{
+						if (x.value != y.value) return x.value < y.value;
+						else return x.name < y.name;
+					}
+			);
+			break;
+		default:
+			sortColumn_ = STOCK_SORT_BY_NAME;
+			std::stable_sort(acct.data.begin(), acct.data.end()
+					, [] (const data_holder& x, const data_holder& y)
+					{
+						return x.name < y.name;
+					}
+			);
+		}
+	}
+
+    mmHTMLBuilder hb;
+    hb.init();
+
+    hb.addHeader(2, _("Summary of Stocks"));
+    hb.addDateNow();
+
+    hb.startTable("95%");
+    for (const auto& acct : stocks)
+	{
+        hb.addTotalRow("", 9, "");
+		hb.addTotalRow(acct.name, 9, "");
+
+        display_header(hb);
+
+		for (const auto& entry : acct.data)
+		{
+            hb.startTableRow();
+			hb.addTableCell(entry.name, false, true);
+			hb.addTableCell(entry.symbol);
+			hb.addTableCell(entry.date);
+			hb.addTableCell(wxString::Format("%.4f", entry.qty), true);
+			hb.addMoneyCell(entry.purchase);
+			hb.addMoneyCell(entry.current);
+			hb.addMoneyCell(entry.commission);
+			hb.addMoneyCell(entry.gainloss);
+			hb.addMoneyCell(entry.value);
+            hb.endTableRow();
+		}
+
+        hb.addRowSeparator(9);
+		hb.addTotalRow(_("Total:"), 8, acct.gainloss);
+		hb.addMoneyCell(acct.total);
+	}
     core_->currencyList_.LoadBaseCurrencySettings();
 
     hb.addRowSeparator(9);
@@ -149,14 +274,41 @@ wxString mmReportSummaryStocks::getHTMLText()
 
 void mmReportSummaryStocks::display_header(mmHTMLBuilder& hb) {
     hb.startTableRow();
-    hb.addTableHeaderCell(_("Name"));
-    hb.addTableHeaderCell(_("Symbol"));
-    hb.addTableHeaderCell(_("Purchase Date"));
-    hb.addTableHeaderCell(_("Quantity"), true);
-    hb.addTableHeaderCell(_("Purchase Price"), true);
-    hb.addTableHeaderCell(_("Current Price"), true);
-    hb.addTableHeaderCell(_("Commission"), true);
-    hb.addTableHeaderCell(_("Gain/Loss"), true);
-    hb.addTableHeaderCell(_("Value"), true);
+	if(STOCK_SORT_BY_NAME == sortColumn_)
+	    hb.addTableHeaderCell(_("Name"));
+	else
+	    hb.addTableHeaderCellLink(wxString::Format("SORT:%d", STOCK_SORT_BY_NAME), _("Name"));
+	if(STOCK_SORT_BY_SYMBOL == sortColumn_)
+		hb.addTableHeaderCell(_("Symbol"));
+	else
+		hb.addTableHeaderCellLink(wxString::Format("SORT:%d", STOCK_SORT_BY_SYMBOL), _("Symbol"));
+	if(STOCK_SORT_BY_DATE == sortColumn_)
+		hb.addTableHeaderCell(_("Purchase Date"));
+	else
+		hb.addTableHeaderCellLink(wxString::Format("SORT:%d", STOCK_SORT_BY_DATE), _("Purchase Date"));
+	if(STOCK_SORT_BY_QTY == sortColumn_)
+		hb.addTableHeaderCell(_("Quantity"), true);
+	else
+		hb.addTableHeaderCellLink(wxString::Format("SORT:%d", STOCK_SORT_BY_QTY), _("Quantity"), true);
+	if(STOCK_SORT_BY_PUR_PRICE == sortColumn_)
+		hb.addTableHeaderCell(_("Purchase Price"), true);
+	else
+		hb.addTableHeaderCellLink(wxString::Format("SORT:%d", STOCK_SORT_BY_PUR_PRICE), _("Purchase Price"), true);
+	if(STOCK_SORT_BY_CUR_PRICE == sortColumn_)
+		hb.addTableHeaderCell(_("Current Price"), true);
+	else
+		hb.addTableHeaderCellLink(wxString::Format("SORT:%d", STOCK_SORT_BY_CUR_PRICE), _("Current Price"), true);
+	if(STOCK_SORT_BY_COMMISSION == sortColumn_)
+		hb.addTableHeaderCell(_("Commission"), true);
+	else
+		hb.addTableHeaderCellLink(wxString::Format("SORT:%d", STOCK_SORT_BY_COMMISSION), _("Commission"), true);
+	if(STOCK_SORT_BY_GAIN_LOSS == sortColumn_)
+		hb.addTableHeaderCell(_("Gain/Loss"), true);
+	else
+		hb.addTableHeaderCellLink(wxString::Format("SORT:%d", STOCK_SORT_BY_GAIN_LOSS), _("Gain/Loss"), true);
+	if(STOCK_SORT_BY_VALUE == sortColumn_)
+		hb.addTableHeaderCell(_("Value"), true);
+	else
+		hb.addTableHeaderCellLink(wxString::Format("SORT:%d", STOCK_SORT_BY_VALUE), _("Value"), true);
     hb.endTableRow();
 }
