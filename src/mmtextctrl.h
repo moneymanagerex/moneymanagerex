8c953c9db6d642a1c1f5240c39aaf2e92bad221d
siena123 <siena123@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-10-06 21:51:47 +0000

    Fix: prefix/suffix currency symbols
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@4763 7f5d68d0-c712-0410-839f-9b116a576cc5

--- src/mmtextctrl.h
+++ src/mmtextctrl.h
@@ -8,22 +8,55 @@
 class mmTextCtrl : public wxTextCtrl
 {
 public:
-    using wxTextCtrl::SetValue;
-public:
     mmTextCtrl() {}
     mmTextCtrl(wxWindow *parent, wxWindowID id, const wxString &value=wxEmptyString
             , const wxPoint &pos=wxDefaultPosition, const wxSize &size=wxDefaultSize
             , long style=0, const wxValidator &validator=wxDefaultValidator, const wxString &name=wxTextCtrlNameStr)
             : wxTextCtrl(parent, id, value, pos, size, style, validator, name)
             {}
+    void SetValue(const wxString &value)
+    {
+        prefix = "";
+        suffix = "";
+        wxTextCtrl::SetValue(value);
+    }
     void SetValue(double value)
     {
         this->SetValue(Model_Currency::toString(value));
+        // update after call to SetValue
+        prefix = Model_Currency::GetBaseCurrency()->PFX_SYMBOL;
+        suffix = Model_Currency::GetBaseCurrency()->SFX_SYMBOL;
     }
     void SetValue(double value, const Model_Account::Data* account)
     {
-       this->SetValue(Model_Account::toString(value, account)); 
+        this->SetValue(Model_Account::toString(value, account));
+        // update after call to SetValue
+        Model_Currency::Data* currency = Model_Currency::instance().get(account->CURRENCYID);
+        prefix = (currency ? currency->PFX_SYMBOL : "");
+        suffix = (currency ? currency->SFX_SYMBOL : "");
     }
+    wxString GetValue() const
+    {
+        // Remove prefix and suffix characters from value
+        wxString val = wxTextCtrl::GetValue();
+        if (!prefix.IsEmpty())
+        {
+            wxString removed;
+            if (val.StartsWith(prefix, &removed))
+                val = removed;
+        }
+        if (!suffix.IsEmpty())
+        {
+            wxString removed;
+            if (val.EndsWith(prefix, &removed))
+                val = removed;
+        }
+        // Base class handles the thousands seperator
+        return val;
+    }
+private:
+    wxString prefix;
+    wxString suffix;
 };
 
 
