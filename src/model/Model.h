ed50b803f29ff1e56abe82dafbd156e5a00e8e80
guanlisheng <guanlisheng@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-10-07 06:20:28 +0000

    cleanup
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@4774 7f5d68d0-c712-0410-839f-9b116a576cc5

--- src/model/Model.h
+++ src/model/Model.h
@@ -1,19 +1,3 @@
-// -*- C++ -*-
-//=============================================================================
-/**
- *      Copyright (c) 2013 Guan Lisheng (guanlisheng@gmail.com)
- *
- *      @file
- *
- *      @author [sqlite2cpp.py]
- *
- *      @brief
- *
- *      Revision History:
- *          AUTO GENERATED at 2013-07-03 22:55:49.491495.
- *          DO NOT EDIT!
- */
-//=============================================================================
 
 #ifndef MODEL_H
 #define MODEL_H
