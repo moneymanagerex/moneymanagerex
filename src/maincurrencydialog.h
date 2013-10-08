dde217d695e22a4e0c9357ca15e93c35d1c45583
vomikan <vomikan@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-10-08 14:30:51 +0000

    cleanup
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@4800 7f5d68d0-c712-0410-839f-9b116a576cc5

--- src/maincurrencydialog.h
+++ src/maincurrencydialog.h
@@ -21,8 +21,8 @@
 
 #include "guiid.h"
 #include "defs.h"
-#include "dbwrapper.h"
 #include <wx/dataview.h>
+#include <map>
 
 #define ID_MYDIALOG8 10040
 #define SYMBOL_MAINCURRENCYDIALOG_STYLE wxCAPTION|wxRESIZE_BORDER|wxSYSTEM_MENU|wxCLOSE_BOX
