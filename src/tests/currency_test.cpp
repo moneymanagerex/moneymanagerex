310614ba576ba5795dd3e90b7d3bd71322fa3bb6
siena123 <siena123@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-10-13 23:30:36 +0000

    use wxWidgets-3.0.0-rc1
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@4849 7f5d68d0-c712-0410-839f-9b116a576cc5

--- src/tests/currency_test.cpp
+++ src/tests/currency_test.cpp
@@ -16,6 +16,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Placeuite 330, Boston, MA  02111-1307  USA
  ********************************************************/
 
+#include "defs.h"
 #include "currency_test.h"
 #include "mmCurrency.h"
 #include "mmCurrencyFormatter.h"
