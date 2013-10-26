5a2f47ac58a005528e221a52a5f59d5adaff5dea
stef145g <stef145g@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-10-26 15:02:46 +0000

    Allow easy generation/installation of DB_Table_xxx.h files
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@4991 7f5d68d0-c712-0410-839f-9b116a576cc5

---
+++ util/build_db_tables.bat
@@ -0,0 +1,23 @@
+@echo off
+REM --------------------------------------------------------------------------
+REM Author : Stefano Giorgio
+REM Purpose: To allow easy installation of DB_Table_xxx.h files
+REM --------------------------------------------------------------------------
+@echo.
+set buildfile_name=sqlite2cpp.py
+set buildfile_location=../sql/tables_v1.sql
+
+set actual_location=%buildfile_location%
+if not exist %actual_location% set actual_location=../%buildfile_location%
+@echo on
+python %buildfile_name% %actual_location%
+@echo off
+if not %buildfile_location%==%actual_location% goto continue
+@echo copy DB_*.* files to src/db
+pause
+copy DB_Table*.* ..\src\db
+del DB_Table*.*
+@echo -------------------------------------------
+:continue
+@echo Build Completed for %buildfile_name%.
+pause
