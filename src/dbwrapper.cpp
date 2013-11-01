0c4cb26d5da5f67f2ce75a4d61de437d6f57e82f
vomikan <vomikan@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-11-01 12:56:56 +0000

    unix eol
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@5059 7f5d68d0-c712-0410-839f-9b116a576cc5

--- src/dbwrapper.cpp
+++ src/dbwrapper.cpp
@@ -1,4 +1,4 @@
-﻿/*******************************************************
+/*******************************************************
  Copyright (C) 2006 Madhan Kanagavel
 
  This program is free software; you can redistribute it and/or modify
--- src/model/Model_Currency.h
+++ src/model/Model_Currency.h
@@ -1,4 +1,4 @@
-﻿/*******************************************************
+/*******************************************************
  Copyright (C) 2013 Guan Lisheng
 
  This program is free software; you can redistribute it and/or modify
