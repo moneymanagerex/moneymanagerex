66d84a74d713e5b346c9be6a53ea1fabcf7cd31e
guanlisheng <guanlisheng@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-11-11 06:07:07 +0000

    revision # fro asset & stock summary
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@5198 7f5d68d0-c712-0410-839f-9b116a576cc5

--- src/reports/summarystocks.h
+++ src/reports/summarystocks.h
@@ -28,6 +28,7 @@ class mmReportSummaryStocks : public mmPrintableBase
 public:
     mmReportSummaryStocks();
     wxString getHTMLText();
+    wxString version() { return "$REV$"; }
 
 private:
     void display_header(mmHTMLBuilder& hb);
