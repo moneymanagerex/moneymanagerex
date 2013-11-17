34b0e939b20962d1ea3f46b2eefdc65c02845aca
siena123 <siena123@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-11-17 03:41:03 +0000

    fix test project
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@5257 7f5d68d0-c712-0410-839f-9b116a576cc5

--- src/tests/currency_test.cpp
+++ src/tests/currency_test.cpp
@@ -18,8 +18,7 @@ Foundation, Inc., 59 Temple Placeuite 330, Boston, MA  02111-1307  USA
 
 #include "defs.h"
 #include "currency_test.h"
-#include "mmCurrency.h"
-#include "mmCurrencyFormatter.h"
+#include "model\Model_Currency.h"
 #include <wx/string.h>
 #include <cppunit/config/SourcePrefix.h>
 
@@ -38,9 +37,6 @@ CurrencyTest::~CurrencyTest()
 void CurrencyTest::setUp()
 {
 	locale_.Init(wxLANGUAGE_ENGLISH);
-
-    mmCurrency currency;
-	CurrencyFormatter::instance().loadSettings(currency);
 }
 
 void CurrencyTest::tearDown()
@@ -49,69 +45,11 @@ void CurrencyTest::tearDown()
 
 void CurrencyTest::formatDoubleToCurrency()
 {
-	double value1 = 1234.1415;
-	double value2 = 0.0099;
+	double value = 0.0099;
 
-	wxString s = CurrencyFormatter::float2Money(0);
-	CPPUNIT_ASSERT(s == wxT("$0.00"));
+    wxString s = Model_Currency::toString(value, 0);
+    CPPUNIT_ASSERT(s == wxT("0.0"));
 
-	s = CurrencyFormatter::float2Money(-0);
-	CPPUNIT_ASSERT(s == wxT("$0.00"));
-
-	s = CurrencyFormatter::float2Money(1.0);
-	CPPUNIT_ASSERT(s == wxT("$1.00"));
-
-	s = CurrencyFormatter::float2Money(-12.3);
-	CPPUNIT_ASSERT(s == wxT("$-12.30"));
-
-	s = CurrencyFormatter::float2Money(123.4);
-	CPPUNIT_ASSERT(s == wxT("$123.40"));
-
-	s = CurrencyFormatter::float2Money(-1000.0);
-	CPPUNIT_ASSERT(s == wxT("$-1,000.00"));
-
-	s = CurrencyFormatter::float2Money(10000.0);
-	CPPUNIT_ASSERT(s == wxT("$10,000.00"));
-
-	s = CurrencyFormatter::float2Money(-100000.0);
-	CPPUNIT_ASSERT(s == wxT("$-100,000.00"));
-
-	s = CurrencyFormatter::float2Money(1000000.0);
-	CPPUNIT_ASSERT(s == wxT("$1,000,000.00"));
-
-	s = CurrencyFormatter::float2Money(-10000000.0);
-	CPPUNIT_ASSERT(s == wxT("$-10,000,000.00"));
-
-	s = CurrencyFormatter::float2Money(100000000.0);
-	CPPUNIT_ASSERT(s == wxT("$100,000,000.00"));
-
-	s = CurrencyFormatter::float2Money(1000000000.0);
-	CPPUNIT_ASSERT(s == wxT("$1,000,000,000.00"));
-
-	s = CurrencyFormatter::float2Money(-10000000000.0);
-	CPPUNIT_ASSERT(s == wxT("$-10,000,000,000.00"));
-
-	s = CurrencyFormatter::float2Money(100000000000.0);
-	CPPUNIT_ASSERT(s == wxT("$100,000,000,000.00"));
-
-	s = CurrencyFormatter::float2Money(-1234567890123.45);
-	CPPUNIT_ASSERT(s == wxT("$-1,234,567,890,123.45"));
-
-	s = CurrencyFormatter::float2Money(value1);
-	CPPUNIT_ASSERT(s == wxT("$1,234.14"));
-
-	s = CurrencyFormatter::float2Money(-value1);
-	CPPUNIT_ASSERT(s == wxT("$-1,234.14"));
-
-	s = CurrencyFormatter::float2Money(value2);
-	CPPUNIT_ASSERT(s == wxT("$0.01"));
-
-	s = CurrencyFormatter::float2Money(-value2);
-	CPPUNIT_ASSERT(s == wxT("$-0.01"));
-
-	s = CurrencyFormatter::float2Money(0.004);
-	CPPUNIT_ASSERT(s == wxT("$0.00"));
-
-	s = CurrencyFormatter::float2Money(0.005);
-	CPPUNIT_ASSERT(s == wxT("$0.01"));
+    s = Model_Currency::toString(-value, 0);
+    CPPUNIT_ASSERT(s == wxT("-0.0"));
 }
