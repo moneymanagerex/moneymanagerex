c46ed793e45e8004f9c5fc220b36b1d8dce5d6d4
siena123 <siena123@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-11-28 16:10:44 +0000

    fix memory leak (by reference)
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@5390 7f5d68d0-c712-0410-839f-9b116a576cc5

--- src/mmcheckingpanel.cpp
+++ src/mmcheckingpanel.cpp
@@ -921,7 +921,7 @@ TransactionListCtrl::TransactionListCtrl(
 //----------------------------------------------------------------------------
 void TransactionListCtrl::createColumns(wxListCtrl &lst)
 {
-    const std::vector<std::tuple<int, wxString, int, bool> > def_data
+    const std::vector<std::tuple<int, wxString&, int, bool> > def_data
     {
         std::make_tuple(COL_DATE      , "      " + _("Date"), 112, false),
         std::make_tuple(COL_NUMBER    ,          _("Number"),  70, false),
