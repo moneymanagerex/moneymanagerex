6aa4588461a7daff97707a4184c6ed28d6876fe4
guanlisheng <guanlisheng@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-12-06 14:23:13 +0000

    update to latest code from github
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@5453 7f5d68d0-c712-0410-839f-9b116a576cc5

--- 3rd/cgitemplate/test.h
+++ 3rd/cgitemplate/test.h
@@ -8,27 +8,24 @@
 #include <vector>
 
 //! test case structure
-struct test_s
-{
- //! template file
- std::string str_file;
- //! error to catch, if any
- std::string str_error_to_catch;
+struct test_s {
+//! template file
+    std::string str_file;
+//! error to catch, if any
+    std::string str_error_to_catch;
 
- //! ctructor
- test_s() {}
- //! ctructor with template file name
- test_s(std::string arg_file)
- {
-  str_file = arg_file;
- }
- //! ctructor with template file name and exception to catch, to test syntax
- // error catching
- test_s(std::string arg_file, std::string str_error)
- {
-  str_file = arg_file;
-  str_error_to_catch = str_error;
- }
+//! ctructor
+    test_s() {}
+//! ctructor with template file name
+    test_s(std::string arg_file) {
+        str_file = arg_file;
+    }
+//! ctructor with template file name and exception to catch, to test syntax
+// error catching
+    test_s(std::string arg_file, std::string str_error) {
+        str_file = arg_file;
+        str_error_to_catch = str_error;
+    }
 };
 
 //! type of container for tests
