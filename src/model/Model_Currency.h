bffbcc8f8ea7646aa6fbf95e99fb01e470707e4c
guanlisheng <guanlisheng@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-12-16 14:22:58 +0000

    add email info
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@5614 7f5d68d0-c712-0410-839f-9b116a576cc5

--- src/model/Model_Currency.h
+++ src/model/Model_Currency.h
@@ -1,5 +1,5 @@
 ﻿/*******************************************************
- Copyright (C) 2013 Guan Lisheng
+ Copyright (C) 2013 Guan Lisheng (guanlisheng@gmail.com)
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
