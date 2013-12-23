468febe84e057df4f0d5df04e8d500675914143c
stef145g <stef145g@7f5d68d0-c712-0410-839f-9b116a576cc5>
2013-12-23 00:13:43 +0000

    Split files: completing split for Model_Checking.h to .h and .cpp files
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@5699 7f5d68d0-c712-0410-839f-9b116a576cc5

--- src/model/Model_Checking.cpp
+++ src/model/Model_Checking.cpp
@@ -58,12 +58,6 @@ wxArrayString Model_Checking::all_status()
     return status;
 }
 
-/** Return the static instance of Model_Checking table */
-Model_Checking& Model_Checking::instance()
-{
-    return Singleton<Model_Checking>::instance();
-}
-
 /**
 * Initialize the global Model_Checking table.
 * Reset the Model_Checking table or create the table if it does not exist.
@@ -78,7 +72,169 @@ Model_Checking& Model_Checking::instance(wxSQLite3Database* db)
     return ins;
 }
 
-Model_Checking::Full_Data::Full_Data(): Data(0), BALANCE(0)
+/** Return the static instance of Model_Checking table */
+Model_Checking& Model_Checking::instance()
+{
+    return Singleton<Model_Checking>::instance();
+}
+
+bool Model_Checking::remove(int id)
+{
+    //TODO: remove all split at once
+    //Model_Splittransaction::instance().remove(Model_Splittransaction::instance().find(Model_Splittransaction::TRANSID(id)));
+    for (const auto& r : Model_Splittransaction::instance().find(Model_Splittransaction::TRANSID(id)))
+        Model_Splittransaction::instance().remove(r.SPLITTRANSID);
+    return this->remove(id, db_);
+}
+
+Model_Splittransaction::Data_Set Model_Checking::splittransaction(const Data* r)
+{
+    return Model_Splittransaction::instance().find(Model_Splittransaction::TRANSID(r->TRANSID));
+}
+
+Model_Splittransaction::Data_Set Model_Checking::splittransaction(const Data& r)
+{
+    return Model_Splittransaction::instance().find(Model_Splittransaction::TRANSID(r.TRANSID));
+}
+
+DB_Table_CHECKINGACCOUNT_V1::TRANSDATE Model_Checking::TRANSDATE(const wxDate& date, OP op)
+{
+    return DB_Table_CHECKINGACCOUNT_V1::TRANSDATE(date.FormatISODate(), op);
+}
+
+DB_Table_CHECKINGACCOUNT_V1::STATUS Model_Checking::STATUS(STATUS_ENUM status, OP op)
+{
+    return DB_Table_CHECKINGACCOUNT_V1::STATUS(toShortStatus(all_status()[status]), op);
+}
+
+DB_Table_CHECKINGACCOUNT_V1::TRANSCODE Model_Checking::TRANSCODE(TYPE type, OP op)
+{
+    return DB_Table_CHECKINGACCOUNT_V1::TRANSCODE(all_type()[type], op);
+}
+
+wxDate Model_Checking::TRANSDATE(const Data* r)
+{
+    return Model::to_date(r->TRANSDATE);
+}
+
+wxDate Model_Checking::TRANSDATE(const Data& r)
+{
+    return Model::to_date(r.TRANSDATE);
+}
+
+Model_Checking::TYPE Model_Checking::type(const Data* r)
+{
+    for (const auto& t : TYPE_CHOICES) if (r->TRANSCODE.CmpNoCase(t.second) == 0) return t.first;
+
+    return WITHDRAWAL;
+}
+
+Model_Checking::TYPE Model_Checking::type(const Data& r)
+{
+    return type(&r);
+}
+
+Model_Checking::STATUS_ENUM Model_Checking::status(const Data* r)
+{
+    if (r->STATUS.CmpNoCase(all_status()[NONE]) == 0)
+        return NONE;
+    else if (r->STATUS.CmpNoCase(all_status()[RECONCILED]) == 0 || r->STATUS.CmpNoCase("R") == 0)
+        return RECONCILED;
+    else if (r->STATUS.CmpNoCase(all_status()[VOID_]) == 0 || r->STATUS.CmpNoCase("V") == 0)
+        return VOID_;
+    else if (r->STATUS.CmpNoCase(all_status()[FOLLOWUP]) == 0 || r->STATUS.CmpNoCase("F") == 0)
+        return FOLLOWUP;
+    else if (r->STATUS.CmpNoCase(all_status()[DUPLICATE_]) == 0 || r->STATUS.CmpNoCase("D") == 0)
+        return DUPLICATE_;
+    else
+        return NONE;
+}
+
+Model_Checking::STATUS_ENUM Model_Checking::status(const Data& r)
+{
+    return status(&r);
+}
+
+double Model_Checking::amount(const Data* r, int account_id)
+{
+    double sum = 0;
+    switch (type(r))
+    {
+    case WITHDRAWAL:
+        sum -= r->TRANSAMOUNT;
+        break;
+    case DEPOSIT:
+        sum += r->TRANSAMOUNT;
+        break;
+    case TRANSFER:
+        if (account_id == r->ACCOUNTID)
+            sum -= r->TRANSAMOUNT;
+        else
+            sum += r->TOTRANSAMOUNT;
+        break;
+    default:
+        break;
+    }
+    return sum;
+}
+
+double Model_Checking::amount(const Data&r, int account_id)
+{
+    return amount(&r, account_id);
+}
+
+double Model_Checking::balance(const Data* r, int account_id)
+{
+    if (Model_Checking::status(r) == Model_Checking::VOID_) return 0;
+    return amount(r, account_id);
+}
+
+double Model_Checking::balance(const Data& r, int account_id)
+{
+    return balance(&r, account_id);
+}
+
+double Model_Checking::withdrawal(const Data* r, int account_id)
+{
+    double bal = balance(r, account_id);
+    return bal <= 0 ? -bal : 0;
+}
+
+double Model_Checking::withdrawal(const Data& r, int account_id)
+{
+    return withdrawal(&r, account_id);
+}
+
+double Model_Checking::deposit(const Data* r, int account_id)
+{
+    double bal = balance(r, account_id);
+    return bal > 0 ? bal : 0;
+}
+
+double Model_Checking::deposit(const Data& r, int account_id)
+{
+    return deposit(&r, account_id);
+}
+
+double Model_Checking::reconciled(const Data* r, int account_id)
+{
+    double bal = balance(r, account_id);
+    return Model_Checking::status(r) == Model_Checking::RECONCILED ? bal : 0;
+}
+
+double Model_Checking::reconciled(const Data& r, int account_id)
+{
+    return reconciled(&r, account_id);
+}
+
+wxString Model_Checking::toShortStatus(const wxString& fullStatus)
+{
+    wxString s = fullStatus.Left(1);
+    s.Replace("N", "");
+    return s;
+}
+
+Model_Checking::Full_Data::Full_Data() : Data(0), BALANCE(0)
 {
 }
 
--- src/model/Model_Checking.h
+++ src/model/Model_Checking.h
@@ -27,11 +27,14 @@ class Model_Checking : public Model<DB_Table_CHECKINGACCOUNT_V1>
 {
 public:
     using Model<DB_Table_CHECKINGACCOUNT_V1>::remove;
+
 public:
     enum TYPE { WITHDRAWAL = 0, DEPOSIT, TRANSFER };
     enum STATUS_ENUM { NONE = 0, RECONCILED, VOID_, FOLLOWUP, DUPLICATE_ };
+
 public:
     static const std::vector<std::pair<TYPE, wxString> > TYPE_CHOICES;
+
 public:
     struct Full_Data: public Data
     {
@@ -51,6 +54,7 @@ public:
         bool has_split() const;
     };
     typedef std::vector<Full_Data> Full_Data_Set;
+
     struct SorterByBALANCE
     { 
         template<class DATA>
@@ -59,6 +63,7 @@ public:
             return x.BALANCE < y.BALANCE;
         }
     };
+
 public:
     Model_Checking();
     ~Model_Checking();
@@ -68,116 +73,45 @@ public:
     static wxArrayString all_status();
 
 public:
-    /** Return the static instance of Model_Checking table */
-    static Model_Checking& instance();
-    
     /**
     * Initialize the global Model_Checking table.
     * Reset the Model_Checking table or create the table if it does not exist.
     */
     static Model_Checking& instance(wxSQLite3Database* db);
 
-public:
-    bool remove(int id)
-    {
-        //TODO: remove all split at once
-        //Model_Splittransaction::instance().remove(Model_Splittransaction::instance().find(Model_Splittransaction::TRANSID(id)));
-        for (const auto& r : Model_Splittransaction::instance().find(Model_Splittransaction::TRANSID(id)))
-            Model_Splittransaction::instance().remove(r.SPLITTRANSID);
-        return this->remove(id, db_);
-    }
-public:
-    static Model_Splittransaction::Data_Set splittransaction(const Data* r)
-    {
-        return Model_Splittransaction::instance().find(Model_Splittransaction::TRANSID(r->TRANSID));
-    }
-    static Model_Splittransaction::Data_Set splittransaction(const Data& r)
-    {
-        return Model_Splittransaction::instance().find(Model_Splittransaction::TRANSID(r.TRANSID));
-    }
-public:
-    static DB_Table_CHECKINGACCOUNT_V1::TRANSDATE TRANSDATE(const wxDate& date, OP op=EQUAL) { return DB_Table_CHECKINGACCOUNT_V1::TRANSDATE(date.FormatISODate(), op); }
-    static DB_Table_CHECKINGACCOUNT_V1::STATUS STATUS(STATUS_ENUM status, OP op = EQUAL) { return DB_Table_CHECKINGACCOUNT_V1::STATUS(toShortStatus(all_status()[status]), op); }
-    static DB_Table_CHECKINGACCOUNT_V1::TRANSCODE TRANSCODE(TYPE type, OP op=EQUAL) { return DB_Table_CHECKINGACCOUNT_V1::TRANSCODE(all_type()[type], op); }
-public:
-    static wxDate TRANSDATE(const Data* r) { return Model::to_date(r->TRANSDATE); }
-    static wxDate TRANSDATE(const Data& r) { return Model::to_date(r.TRANSDATE); }
-    static TYPE type(const Data* r)
-    {
-        for (const auto& t: TYPE_CHOICES) if (r->TRANSCODE.CmpNoCase(t.second) == 0) return t.first;
+    /** Return the static instance of Model_Checking table */
+    static Model_Checking& instance();
 
-        return WITHDRAWAL;
-    }
-    static TYPE type(const Data& r) { return type(&r); }
-    static STATUS_ENUM status(const Data* r)
-    {
-        if (r->STATUS.CmpNoCase(all_status()[NONE]) == 0)
-            return NONE;
-        else if (r->STATUS.CmpNoCase(all_status()[RECONCILED]) == 0 || r->STATUS.CmpNoCase("R") == 0)
-            return RECONCILED;
-        else if (r->STATUS.CmpNoCase(all_status()[VOID_]) == 0 || r->STATUS.CmpNoCase("V") == 0)
-            return VOID_;
-        else if (r->STATUS.CmpNoCase(all_status()[FOLLOWUP]) == 0 || r->STATUS.CmpNoCase("F") == 0)
-            return FOLLOWUP;
-        else if (r->STATUS.CmpNoCase(all_status()[DUPLICATE_]) == 0 || r->STATUS.CmpNoCase("D") == 0)
-            return DUPLICATE_;
-        else 
-            return NONE;
-    }
-    static STATUS_ENUM status(const Data& r) { return status(&r); }
-    static double amount(const Data* r, int account_id = -1)
-    {
-        double sum = 0;
-        switch (type(r))
-        {
-        case WITHDRAWAL:
-            sum -= r->TRANSAMOUNT;
-            break;
-        case DEPOSIT:
-            sum += r->TRANSAMOUNT;
-            break;
-        case TRANSFER:
-            if (account_id == r->ACCOUNTID)
-                sum -= r->TRANSAMOUNT;
-            else
-                sum += r->TOTRANSAMOUNT;
-            break;
-        default:
-            break;
-        }
-        return sum;
-    }
-    static double amount(const Data&r, int account_id = -1) { return amount(&r, account_id); }
-    static double balance(const Data* r, int account_id = -1)
-    {
-        if (Model_Checking::status(r) == Model_Checking::VOID_) return 0;
-        return amount(r, account_id);
-    }
-    static double balance(const Data& r, int account_id = -1) { return balance(&r, account_id); }
-    static double withdrawal(const Data* r, int account_id = -1)
-    {
-        double bal = balance(r, account_id);
-        return bal <= 0 ? -bal : 0;
-    }
-    static double withdrawal(const Data& r, int account_id) { return withdrawal(&r, account_id); };
-    static double deposit(const Data* r, int account_id)
-    {
-        double bal = balance(r, account_id);
-        return bal > 0 ? bal : 0;
-    }
-    static double deposit(const Data& r, int account_id) { return deposit(&r, account_id); }
-    static double reconciled(const Data* r, int account_id) 
-    {
-        double bal = balance(r, account_id);
-        return Model_Checking::status(r) == Model_Checking::RECONCILED ? bal : 0;
-    }
-    static double reconciled(const Data& r, int account_id) { return reconciled(&r, account_id); }
-    static wxString toShortStatus(const wxString& fullStatus)
-    {
-        wxString s = fullStatus.Left(1);
-        s.Replace("N", "");
-        return s;
-    }
+public:
+    bool remove(int id);
+
+public:
+    static Model_Splittransaction::Data_Set splittransaction(const Data* r);
+    static Model_Splittransaction::Data_Set splittransaction(const Data& r);
+
+public:
+    static DB_Table_CHECKINGACCOUNT_V1::TRANSDATE TRANSDATE(const wxDate& date, OP op = EQUAL);
+    static DB_Table_CHECKINGACCOUNT_V1::STATUS STATUS(STATUS_ENUM status, OP op = EQUAL);
+    static DB_Table_CHECKINGACCOUNT_V1::TRANSCODE TRANSCODE(TYPE type, OP op = EQUAL);
+
+public:
+    static wxDate TRANSDATE(const Data* r);
+    static wxDate TRANSDATE(const Data& r);
+    static TYPE type(const Data* r);
+    static TYPE type(const Data& r);
+    static STATUS_ENUM status(const Data* r);
+    static STATUS_ENUM status(const Data& r);
+    static double amount(const Data* r, int account_id = -1);
+    static double amount(const Data&r, int account_id = -1);
+    static double balance(const Data* r, int account_id = -1);
+    static double balance(const Data& r, int account_id = -1);
+    static double withdrawal(const Data* r, int account_id = -1);
+    static double withdrawal(const Data& r, int account_id);
+    static double deposit(const Data* r, int account_id);
+    static double deposit(const Data& r, int account_id);
+    static double reconciled(const Data* r, int account_id);
+    static double reconciled(const Data& r, int account_id);
+    static wxString toShortStatus(const wxString& fullStatus);
 };
 
 #endif // 
