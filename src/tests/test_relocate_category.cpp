f6c2638ca3b25e6d122272b35cdbce16163ecd02
stef145g <stef145g@7f5d68d0-c712-0410-839f-9b116a576cc5>
2014-01-13 14:43:15 +0000

    Tests: Updated
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@5907 7f5d68d0-c712-0410-839f-9b116a576cc5

--- src/tests/test_relocate_category.cpp
+++ src/tests/test_relocate_category.cpp
@@ -77,6 +77,45 @@ void Test_Relocate_Category::setUp()
     // settings table in the main database.
     Model_Setting::instance(&m_test_db);
     mmIniOptions::instance().loadOptions();
+
+    int cat_id_insurance = Model_Category::instance().get("Insurance")->id();
+    int subcat_id_auto = Model_Subcategory::instance().get("Auto", cat_id_insurance)->id();
+
+    Model_Checking::instance().Begin();
+    {
+        // Set up the categories in the other tables.
+        Model_Checking::Data* checking_entry = Model_Checking::instance().create();
+        checking_entry->CATEGID = cat_id_insurance;
+        checking_entry->SUBCATEGID = subcat_id_auto;
+        Model_Checking::instance().save(checking_entry);
+
+        Model_Splittransaction::Data* split_trans_entry = Model_Splittransaction::instance().create();
+        split_trans_entry->CATEGID = cat_id_insurance;
+        split_trans_entry->SUBCATEGID = subcat_id_auto;
+        Model_Splittransaction::instance().save(split_trans_entry);
+
+        Model_Budgetsplittransaction::Data* budget_split_trans_entry = Model_Budgetsplittransaction::instance().create();
+        budget_split_trans_entry->CATEGID = cat_id_insurance;
+        budget_split_trans_entry->SUBCATEGID = subcat_id_auto;
+        Model_Budgetsplittransaction::instance().save(budget_split_trans_entry);
+
+        Model_Billsdeposits::Data* bill_entry = Model_Billsdeposits::instance().create();
+        bill_entry->CATEGID = cat_id_insurance;
+        bill_entry->SUBCATEGID = subcat_id_auto;
+        Model_Billsdeposits::instance().save(bill_entry);
+
+        Model_Budget::Data* budget_entry = Model_Budget::instance().create();
+        budget_entry->CATEGID = cat_id_insurance;
+        budget_entry->SUBCATEGID = subcat_id_auto;
+        Model_Budget::instance().save(budget_entry);
+
+        Model_Payee::Data* payee_entry = Model_Payee::instance().create();
+        payee_entry->PAYEENAME = "Aldi";
+        payee_entry->CATEGID = cat_id_insurance;
+        payee_entry->SUBCATEGID = subcat_id_auto;
+        Model_Payee::instance().save(payee_entry);
+    }
+    Model_Checking::instance().Commit();
 }
 
 void Test_Relocate_Category::tearDown()
@@ -93,10 +132,34 @@ void Test_Relocate_Category::ShowMessage(wxString msg)
 
 void Test_Relocate_Category::test_dialog()
 {
-    ShowMessage("Not complete:\n\nNo data in tables.\n");
+    ShowMessage("Please relocate Insurance/Auto to Automobile/Registration\n\nThis should result in 6 records being changed.\n");
     relocateCategoryDialog dlg(m_frame);
     if (dlg.ShowModal() == wxID_OK)
     {
-        ShowMessage("To be completed.");
+        wxString msg = "Test Complete: ";
+        msg << dlg.updatedCategoriesCount() << " records changed.";
+        ShowMessage(msg);
     }
+
+    int cat_id_auto = Model_Category::instance().get("Automobile")->id();
+    int subcat_id_reg = Model_Subcategory::instance().get("Registration", cat_id_auto)->id();
+
+    CPPUNIT_ASSERT(dlg.updatedCategoriesCount() == 6);
+    CPPUNIT_ASSERT(Model_Checking::instance().get(1)->CATEGID == cat_id_auto);
+    CPPUNIT_ASSERT(Model_Checking::instance().get(1)->SUBCATEGID == subcat_id_reg);
+
+    CPPUNIT_ASSERT(Model_Splittransaction::instance().get(1)->CATEGID == cat_id_auto);
+    CPPUNIT_ASSERT(Model_Splittransaction::instance().get(1)->SUBCATEGID == subcat_id_reg);
+
+    CPPUNIT_ASSERT(Model_Budgetsplittransaction::instance().get(1)->CATEGID == cat_id_auto);
+    CPPUNIT_ASSERT(Model_Budgetsplittransaction::instance().get(1)->SUBCATEGID == subcat_id_reg);
+
+    CPPUNIT_ASSERT(Model_Billsdeposits::instance().get(1)->CATEGID == cat_id_auto);
+    CPPUNIT_ASSERT(Model_Billsdeposits::instance().get(1)->SUBCATEGID == subcat_id_reg);
+
+    CPPUNIT_ASSERT(Model_Budget::instance().get(1)->CATEGID == cat_id_auto);
+    CPPUNIT_ASSERT(Model_Budget::instance().get(1)->SUBCATEGID == subcat_id_reg);
+
+    CPPUNIT_ASSERT(Model_Payee::instance().get("Aldi")->CATEGID == cat_id_auto);
+    CPPUNIT_ASSERT(Model_Payee::instance().get("Aldi")->SUBCATEGID == subcat_id_reg);
 }
