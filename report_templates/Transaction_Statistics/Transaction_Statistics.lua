12162345c1cce096600045fb9195b27a352d7099
vomikan <vomikan@7f5d68d0-c712-0410-839f-9b116a576cc5>
2014-02-09 16:18:05 +0000

    Transaction statistics report update
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@6104 7f5d68d0-c712-0410-839f-9b116a576cc5

--- report_templates/Transaction_Statistics/Transaction_Statistics.lua
+++ report_templates/Transaction_Statistics/Transaction_Statistics.lua
@@ -1,16 +1,16 @@
-local totals = {0,0,0,0,0,0,0,0,0,0,0,0}
+local totals = {0,0,0,0,0,0,0,0,0,0,0,0};
 local grand_total = 0; local count = 0;
 local months = {"Jan", "Feb", "Mar" , "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"}
 
 function handle_record(record)
-    local year_total = 0
+    local year_total = 0;
     for i=1,12 do
         year_total = year_total +  record:get(months[i]);
         record:set(months[i], math.floor(record:get(months[i])));
         totals[i] = totals[i] + record:get(months[i]);
     end
     count = count +1;
-    record:set("YEAR_TOTAL", year_total)
+    record:set("YEAR_TOTAL", year_total);
     grand_total = grand_total + year_total;
 end
 
