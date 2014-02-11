dc9365d9a578aea2f5d348946bdcd3a41d5adb28
vomikan <vomikan@7f5d68d0-c712-0410-839f-9b116a576cc5>
2014-02-11 22:16:56 +0000

    typo
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@6117 7f5d68d0-c712-0410-839f-9b116a576cc5

--- src/mmTips.h
+++ src/mmTips.h
@@ -3,7 +3,7 @@ static const wxString TIPS[] =
 {
     wxTRANSLATE("Recommendation: Always backup your .mmb database file regularly."),
     wxTRANSLATE("Recommendation: If upgrading to a new version of MMEX, make sure you backup your .mmb database file before doing so."),
-    wxTRANSLATE("Recommendation: Use copy (Ctrl+Ñ) and paste (Ctrl+V) for frequently used transactions."),
+    wxTRANSLATE("Recommendation: Use copy (Ctrl+C) and paste (Ctrl+V) for frequently used transactions."),
     wxTRANSLATE("Tip: Remember to make backups of your .mmb."),
     wxTRANSLATE("Tip: The .mmb file is not encrypted. That means anyone else having the proper know how can actually open the file and read the contents. So make sure that if you are storing any sensitive financial information it is properly guarded."),
     wxTRANSLATE("Tip: To mark a transaction as reconciled, just select the transaction and hit the 'r' or 'R' key. To mark a transaction as unreconciled, just select the transaction and hit the 'u' or 'U' key."),
