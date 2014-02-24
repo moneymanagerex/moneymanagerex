d1fca2b36e6186334df8f7ebe1a851cfebc3820d
guanlisheng <guanlisheng@7f5d68d0-c712-0410-839f-9b116a576cc5>
2014-02-24 09:29:29 +0000

    update copyright to include 2014
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@6165 7f5d68d0-c712-0410-839f-9b116a576cc5

--- src/model/Model_Budget.h
+++ src/model/Model_Budget.h
@@ -1,5 +1,5 @@
 /*******************************************************
- Copyright (C) 2013 James Higley
+ Copyright (C) 2013,2014 James Higley
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
