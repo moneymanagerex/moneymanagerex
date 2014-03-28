98c320cbfe5ce84501d4a14a1331a62e644fbe96
guanlisheng <guanlisheng@7f5d68d0-c712-0410-839f-9b116a576cc5>
2014-03-28 01:10:40 +0000

    copyright info
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@6259 7f5d68d0-c712-0410-839f-9b116a576cc5

--- src/webserver.h
+++ src/webserver.h
@@ -1,5 +1,6 @@
 /*******************************************************
  Copyright (C) 2014 James Higley
+ Copyright (C) 2014 Guan Lisheng (guanlisheng@gmail.com)
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
