3245c948b35518874858a3fce2145cacdf4d19af
stef145g <stef145g@7f5d68d0-c712-0410-839f-9b116a576cc5>
2014-05-10 17:36:53 +0000

    Fix Tests Build, Update msvc-2013e project files
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@6564 7f5d68d0-c712-0410-839f-9b116a576cc5

--- src/tests/test_checking.cpp
+++ src/tests/test_checking.cpp
@@ -26,7 +26,6 @@ Foundation, Inc., 59 Temple Placeuite 330, Boston, MA  02111-1307  USA
 #include "test_checking.h"
 #include "transdialog.h"
 #include "mmcheckingpanel.h"
-#include "mmHook.h"
 
 /*****************************************************************************
 Turn test ON or OFF in file: defined_test_selection.h
