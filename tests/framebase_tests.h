9bae4f848faba7cf27195f58b4945d1d0400cade
guanlisheng <guanlisheng@7f5d68d0-c712-0410-839f-9b116a576cc5>
2014-05-15 15:01:33 +0000

    use capital regular
    git-svn-id: https://svn.code.sf.net/p/moneymanagerex/code/trunk@6589 7f5d68d0-c712-0410-839f-9b116a576cc5

--- tests/framebase_tests.h
+++ tests/framebase_tests.h
@@ -16,7 +16,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Placeuite 330, Boston, MA  02111-1307  USA
  ********************************************************/
 
-#include "../mmHook.h"
+#include "mmHook.h"
 //----------------------------------------------------------------------------
 class wxFrame;
 class wxInfoBar;
