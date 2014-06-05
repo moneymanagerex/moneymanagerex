b85f948d6d93add03f11990056b56e76cb64ccec
thehobbit <thehobbit78@hotmail.com>
2014-06-05 12:16:36 +0200

    bugfix: a stock price was added everytime, not updated if already present

--- src/stockdialog.cpp
+++ src/stockdialog.cpp
@@ -434,6 +434,12 @@ void mmStockDialog::OnOk(wxCommandEvent& /*event*/)
 
     // update stock history table
     Model_StockHistory::Data *stockHist = Model_StockHistory::instance().create();
+    DB_Table_STOCKHISTORY_V1::Data_Set histData = Model_StockHistory::instance().search(transID_, false, 0, priceDate_->GetValue(), priceDate_->GetValue());
+    for (const auto& hist : histData)
+    {
+        *stockHist = hist;
+        break;
+    }
     stockHist->STOCKID = transID_;
     stockHist->DATE = priceDate_->GetValue().FormatISODate();
     stockHist->VALUE = cPrice;
