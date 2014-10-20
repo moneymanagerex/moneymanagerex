204e3c08412061c8ad20afd19dc5bbf4e6aa7b45
vomikan <vomikan@mail.ru>
2014-10-20 14:49:15 +0400

    Users Colors saved to finDB not iniDB

--- src/model/Model_Setting.h
+++ src/model/Model_Setting.h
@@ -44,6 +44,15 @@ public:
     */
     static Model_Setting& instance();
 
+    void Begin()
+    {
+        this->db_->Begin();
+    }
+    void Commit()
+    {
+        this->db_->Commit();
+    }
+
 public:
     // Setter
     void Set(const wxString& key, int value);
@@ -56,7 +65,6 @@ public:
     bool GetBoolSetting(const wxString& key, bool default_value);
     int GetIntSetting(const wxString& key, int default_value);
 
-    wxColour GetColourSetting(const wxString& key, const wxColour& default_value = wxColour(255, 255, 255));
     wxString GetStringSetting(const wxString& key, const wxString& default_value);
     wxString getLastDbPath();
 
