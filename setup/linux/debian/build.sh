166856251f12bf0358a3af7a7994d82e9af35bef
David Jones <dwrj87@gmail.com>
2015-02-12 12:03:32 +0000

    refactor(linux.packaging). Created common variables

--- setup/linux/debian/build.sh
+++ setup/linux/debian/build.sh
@@ -10,17 +10,10 @@
 #
 
 # Specify system Architecture  ("i386" or "amd64")
+
+. ../common/variables.sh
+
 ARCHITECTURE="amd64"
-
-# Specify the build version of mmex
-MMEX_VERSION="1.2.0"
-MMEX_RELEASE_DATE="2015-02-09"
-EMAIL="moneymanagerex@moneymanagerex.org"
-HOMEPAGE="http://www.moneymanagerex.org"
-BUILD_DIR="$HOME/build"
-
-
-
 
 PACKAGE_NAME="mmex-$MMEX_VERSION-$ARCHITECTURE"
 
@@ -56,13 +49,15 @@ gzip -9 -f $BUILD_DIR/$PACKAGE_NAME/usr/share/doc/mmex/changelog
 
 #Copyright
 cp $BUILD_DIR/$PACKAGE_NAME/usr/share/doc/mmex/contrib.txt $BUILD_DIR/$PACKAGE_NAME/usr/share/doc/mmex/copyright
+sed -i "s/See the GNU General Public License for more details./A copy of the GPLv2 can be found in \"\/usr\/share\/common-licenses\/GPL-2\"/g" $BUILD_DIR/$PACKAGE_NAME/usr/share/doc/mmex/copyright
+
 
 #Manpage
-cp $MMEX_DIR/setup/linux/debian/mmex.1 $BUILD_DIR/$PACKAGE_NAME/usr/share/man/man1/mmex.1
+cp $MMEX_DIR/setup/linux/common/mmex.1 $BUILD_DIR/$PACKAGE_NAME/usr/share/man/man1/mmex.1
 sed -i "s/MMEX_RELEASE_DATE/$MMEX_RELEASE_DATE/g" $BUILD_DIR/$PACKAGE_NAME/usr/share/man/man1/mmex.1
 sed -i "s/MMEX_VERSION/$MMEX_VERSION/g" $BUILD_DIR/$PACKAGE_NAME/usr/share/man/man1/mmex.1
 gzip -9 -f $BUILD_DIR/$PACKAGE_NAME/usr/share/man/man1/mmex.1
-chmod 0644 $BUILD_DIR/$PACKAGE_NAME/usr/share/man/man1/mmex.1.gz
+chmod 644 $BUILD_DIR/$PACKAGE_NAME/usr/share/man/man1/mmex.1.gz
 
 #Calculate installed size
 INSTALLED_SIZE=$(du -sb $BUILD_DIR/ | cut -f1)
@@ -75,19 +70,11 @@ Version: $MMEX_VERSION
 Section: misc
 Priority: extra
 Architecture: $ARCHITECTURE
-Homepage: $HOMEPAGE
-Depends: libc6 (>= 2.3.5-1), libwxgtk3.0-0 (>= 3.0.0)
+Homepage: $MMEX_HOMEPAGE
+Depends: $MMEX_DEPENDS
 Installed-Size: $INSTALLED_SIZE
-Maintainer: MoneyManagerEx <$EMAIL>
-Description: Simple to use financial management software
- Money Manager Ex (MMEX) is a free, open-source,
- cross-platform, easy-to-use personal finance software.
- It primarily helps organize one's finances and keeps
- track of where, when and how the money goes.
- MMEX includes all the basic features that 90% of users
- would want to see in a personal finance application.
- The design goals are to concentrate on simplicity
- and user friendliness - something one can use everyday." > $BUILD_DIR/$PACKAGE_NAME/DEBIAN/control
+Maintainer: MoneyManagerEx <$MMEX_EMAIL>
+Description: $MMEX_DESCRIPTION" > $BUILD_DIR/$PACKAGE_NAME/DEBIAN/control
 
 cd $BUILD_DIR
 fakeroot dpkg-deb -b $PACKAGE_NAME
