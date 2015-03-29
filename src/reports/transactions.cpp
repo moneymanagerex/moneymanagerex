0643ba4a349cc5efe3e17d5ab2a6f34d36a009f3
Stefano <stef145g@gmail.com>
2015-03-29 15:48:13 +1100

    feat(Asset.Stock.TransferTrans): update transactions report
    place a *Tr against the transaction type
    when the transaction is a transfer from stocks or assets

--- src/reports/transactions.cpp
+++ src/reports/transactions.cpp
@@ -86,7 +86,14 @@ wxString mmReportTransactions::getHTMLText()
         hb.addTableCell(transaction.PAYEENAME);
         hb.addTableCell(transaction.STATUS);
         hb.addTableCell(transaction.CATEGNAME);
-        hb.addTableCell(wxGetTranslation(transaction.TRANSCODE));
+        if (Model_Checking::foreignTransactionAsTransfer(transaction))
+        {
+            hb.addTableCell(wxGetTranslation(transaction.TRANSCODE) + " *Tr");
+        }
+        else
+        {
+            hb.addTableCell(wxGetTranslation(transaction.TRANSCODE));
+        }
         hb.addTableCell(transaction.TRANSACTIONNUMBER);
         hb.addTableCell(transaction.NOTES);
         // Get the exchange rate for the account
