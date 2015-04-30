f8135d114bd5da31895c6ed9f5e1e57b5bee5ba5
vomikan <nikolay.akimov@gmail.com>
2015-04-30 13:14:56 +0300

    fix(Images): crash fix on GTK

--- src/images_list.cpp
+++ src/images_list.cpp
@@ -57,54 +57,55 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include "../resources/work.xpm"
 #include "../resources/yandex_money.xpm"
 
-const std::map<int, wxBitmap> images_list =
+static const std::map<int, wxBitmap> images_list()
 {
-      { HOUSE_XPM, wxBitmap(wxImage(house_xpm).Scale(16, 16)) }
-    , { MONEYACCOUNT2_XPM, wxBitmap(wxImage(moneyaccount_xpm).Scale(16, 16)) }
-    , { SCHEDULE_XPM, wxBitmap(wxImage(schedule_xpm).Scale(16, 16)) }
-    , { CALENDAR_XPM, wxBitmap(wxImage(calendar_xpm).Scale(16, 16)) }
-    , { PIECHART_XPM, wxBitmap(wxImage(chartpiereport_xpm).Scale(16, 16)) }
-    , { HELP_XPM, wxBitmap(wxImage(help_xpm).Scale(16, 16)) }
-    , { FILTER_XPM, wxBitmap(wxImage(filter_xpm).Scale(16, 16)) }
-    , { CAR_XPM, wxBitmap(wxImage(car_xpm).Scale(16, 16)) }
-    , { CUSTOMSQL_XPM, wxBitmap(wxImage(customsql_xpm).Scale(16, 16)) }
-    , { CUSTOMSQL_GRP_XPM, wxBitmap(wxImage(customsql_xpm).Scale(16, 16)) } //TODO: GRM rep group ico
-    , { MONEYACCOUNT_XPM, wxBitmap(wxImage(moneyaccount_xpm).Scale(16, 16)) }
-    , { SAVINGS_ACC_FAVORITE_XPM, wxBitmap(wxImage(savings_acc_favorite_xpm).Scale(16, 16)) }
-    , { SAVINGS_ACC_CLOSED_XPM, wxBitmap(wxImage(savings_acc_closed_xpm).Scale(16, 16)) }
-    , { TERMACCOUNT_XPM, wxBitmap(wxImage(termaccount_xpm).Scale(16, 16)) }
-    , { TERM_ACC_FAVORITE_XPM, wxBitmap(wxImage(term_acc_favorite_xpm).Scale(16, 16)) }
-    , { TERM_ACC_CLOSED_XPM, wxBitmap(wxImage(term_acc_closed_xpm).Scale(16, 16)) }
-    , { STOCK_ACC_XPM, wxBitmap(wxImage(stock_acc_xpm).Scale(16, 16)) }
-    , { STOCK_ACC_FAVORITE_XPM, wxBitmap(wxImage(stock_acc_favorite_xpm).Scale(16, 16)) }
-    , { STOCK_ACC_CLOSED_XPM, wxBitmap(wxImage(stock_acc_closed_xpm).Scale(16, 16)) }
-    , { CARD_ACC_XPM, wxBitmap(wxImage(card_acc_xpm).Scale(16, 16)) }
-    , { CARD_ACC_FAVORITE_XPM, wxBitmap(wxImage(card_acc_favorite_xpm).Scale(16, 16)) }
-    , { CARD_ACC_CLOSED_XPM, wxBitmap(wxImage(card_acc_closed_xpm).Scale(16, 16)) }
-    // Custom icons for accounts
-    , { MONEY_DOLLAR_XPM, wxBitmap(wxImage(money_dollar_xpm).Scale(16, 16)) }
-    , { MONEY_EURO_XPM, wxBitmap(wxImage(money_euro_xpm).Scale(16, 16)) }
-    , { FLAG_XPM, wxBitmap(wxImage(flag_xpm).Scale(16, 16)) }
-    , { ACCOUNTTREE_XPM, wxBitmap(wxImage(accounttree_xpm).Scale(16, 16)) }
-    , { ABOUT_XPM, wxBitmap(wxImage(about_xpm).Scale(16, 16)) }
-    , { CLOCK_XPM, wxBitmap(wxImage(clock_xpm).Scale(16, 16)) }
-    , { CAT_XPM, wxBitmap(wxImage(cat_xpm).Scale(16, 16)) }
-    , { DOG_XPM, wxBitmap(wxImage(dog_xpm).Scale(16, 16)) }
-    , { TREES_XPM, wxBitmap(wxImage(trees_xpm).Scale(16, 16)) }
-    , { HOURGLASS_XPM, wxBitmap(wxImage(hourglass_xpm).Scale(16, 16)) }
-    , { WORK_XPM, wxBitmap(wxImage(work_xpm).Scale(16, 16)) }
-    , { YANDEX_MONEY_XPM, wxBitmap(wxImage(yandex_money_xpm).Scale(16, 16)) }
-    , { WEB_MONEY_XPM, wxBitmap(wxImage(web_money_xpm).Scale(16, 16)) }
-    , { RUBIK_CUBE_XPM, wxBitmap(wxImage(rubik_cube_xpm).Scale(16, 16)) }
+    return {
+        { HOUSE_XPM, wxBitmap(wxImage(house_xpm).Scale(16, 16)) }
+        , { MONEYACCOUNT2_XPM, wxBitmap(wxImage(moneyaccount_xpm).Scale(16, 16)) }
+        , { SCHEDULE_XPM, wxBitmap(wxImage(schedule_xpm).Scale(16, 16)) }
+        , { CALENDAR_XPM, wxBitmap(wxImage(calendar_xpm).Scale(16, 16)) }
+        , { PIECHART_XPM, wxBitmap(wxImage(chartpiereport_xpm).Scale(16, 16)) }
+        , { HELP_XPM, wxBitmap(wxImage(help_xpm).Scale(16, 16)) }
+        , { FILTER_XPM, wxBitmap(wxImage(filter_xpm).Scale(16, 16)) }
+        , { CAR_XPM, wxBitmap(wxImage(car_xpm).Scale(16, 16)) }
+        , { CUSTOMSQL_XPM, wxBitmap(wxImage(customsql_xpm).Scale(16, 16)) }
+        , { CUSTOMSQL_GRP_XPM, wxBitmap(wxImage(customsql_xpm).Scale(16, 16)) } //TODO: GRM rep group ico
+        , { MONEYACCOUNT_XPM, wxBitmap(wxImage(moneyaccount_xpm).Scale(16, 16)) }
+        , { SAVINGS_ACC_FAVORITE_XPM, wxBitmap(wxImage(savings_acc_favorite_xpm).Scale(16, 16)) }
+        , { SAVINGS_ACC_CLOSED_XPM, wxBitmap(wxImage(savings_acc_closed_xpm).Scale(16, 16)) }
+        , { TERMACCOUNT_XPM, wxBitmap(wxImage(termaccount_xpm).Scale(16, 16)) }
+        , { TERM_ACC_FAVORITE_XPM, wxBitmap(wxImage(term_acc_favorite_xpm).Scale(16, 16)) }
+        , { TERM_ACC_CLOSED_XPM, wxBitmap(wxImage(term_acc_closed_xpm).Scale(16, 16)) }
+        , { STOCK_ACC_XPM, wxBitmap(wxImage(stock_acc_xpm).Scale(16, 16)) }
+        , { STOCK_ACC_FAVORITE_XPM, wxBitmap(wxImage(stock_acc_favorite_xpm).Scale(16, 16)) }
+        , { STOCK_ACC_CLOSED_XPM, wxBitmap(wxImage(stock_acc_closed_xpm).Scale(16, 16)) }
+        , { CARD_ACC_XPM, wxBitmap(wxImage(card_acc_xpm).Scale(16, 16)) }
+        , { CARD_ACC_FAVORITE_XPM, wxBitmap(wxImage(card_acc_favorite_xpm).Scale(16, 16)) }
+        , { CARD_ACC_CLOSED_XPM, wxBitmap(wxImage(card_acc_closed_xpm).Scale(16, 16)) }
+        // Custom icons for accounts
+        , { MONEY_DOLLAR_XPM, wxBitmap(wxImage(money_dollar_xpm).Scale(16, 16)) }
+        , { MONEY_EURO_XPM, wxBitmap(wxImage(money_euro_xpm).Scale(16, 16)) }
+        , { FLAG_XPM, wxBitmap(wxImage(flag_xpm).Scale(16, 16)) }
+        , { ACCOUNTTREE_XPM, wxBitmap(wxImage(accounttree_xpm).Scale(16, 16)) }
+        , { ABOUT_XPM, wxBitmap(wxImage(about_xpm).Scale(16, 16)) }
+        , { CLOCK_XPM, wxBitmap(wxImage(clock_xpm).Scale(16, 16)) }
+        , { CAT_XPM, wxBitmap(wxImage(cat_xpm).Scale(16, 16)) }
+        , { DOG_XPM, wxBitmap(wxImage(dog_xpm).Scale(16, 16)) }
+        , { TREES_XPM, wxBitmap(wxImage(trees_xpm).Scale(16, 16)) }
+        , { HOURGLASS_XPM, wxBitmap(wxImage(hourglass_xpm).Scale(16, 16)) }
+        , { WORK_XPM, wxBitmap(wxImage(work_xpm).Scale(16, 16)) }
+        , { YANDEX_MONEY_XPM, wxBitmap(wxImage(yandex_money_xpm).Scale(16, 16)) }
+        , { WEB_MONEY_XPM, wxBitmap(wxImage(web_money_xpm).Scale(16, 16)) }
+        , { RUBIK_CUBE_XPM, wxBitmap(wxImage(rubik_cube_xpm).Scale(16, 16)) }
+    };
 };
 
 wxImageList* navtree_images_list()
 {
     wxImageList* imageList = new wxImageList(16, 16);
-    for (const auto& img : images_list)
+    for (const auto& img : images_list())
     {
         imageList->Add(img.second);
     }
     return imageList;
 }
-
