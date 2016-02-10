d47352b730f10a40844cf5e7a1c301d60b9d8745
Gabriele-V <gabriele@villanet.it>
2016-02-10 20:45:19 +0100

    fix(style): replace tabs with spaces in all files

--- src/model/Model_Attachment.cpp
+++ src/model/Model_Attachment.cpp
@@ -21,12 +21,12 @@
 
 const std::vector<std::pair<Model_Attachment::REFTYPE, wxString> > Model_Attachment::REFTYPE_CHOICES =
 {
-	std::make_pair(Model_Attachment::TRANSACTION, wxString("Transaction")),
-	std::make_pair(Model_Attachment::STOCK, wxString("Stock")),
-	std::make_pair(Model_Attachment::ASSET, wxString("Asset")),
-	std::make_pair(Model_Attachment::BANKACCOUNT, wxString("BankAccount")),
-	std::make_pair(Model_Attachment::BILLSDEPOSIT, wxString("RecurringTransaction")),
-	std::make_pair(Model_Attachment::PAYEE, wxString("Payee"))
+    std::make_pair(Model_Attachment::TRANSACTION, wxString("Transaction")),
+    std::make_pair(Model_Attachment::STOCK, wxString("Stock")),
+    std::make_pair(Model_Attachment::ASSET, wxString("Asset")),
+    std::make_pair(Model_Attachment::BANKACCOUNT, wxString("BankAccount")),
+    std::make_pair(Model_Attachment::BILLSDEPOSIT, wxString("RecurringTransaction")),
+    std::make_pair(Model_Attachment::PAYEE, wxString("Payee"))
 };
 
 Model_Attachment::Model_Attachment()
@@ -61,19 +61,19 @@ Model_Attachment& Model_Attachment::instance()
 /** Return a dataset with attachments linked to a specific object */
 const Model_Attachment::Data_Set Model_Attachment::FilterAttachments(const wxString& RefType, const int RefId)
 {
-	Data_Set attachments;
-	for (auto &attachment : this->all(COL_DESCRIPTION))
-	{
-		if (attachment.REFTYPE.Lower().Matches(RefType.Lower().Append("*")) && attachment.REFID == RefId)
-			attachments.push_back(attachment);
-	}
-	return attachments;
+    Data_Set attachments;
+    for (auto &attachment : this->all(COL_DESCRIPTION))
+    {
+        if (attachment.REFTYPE.Lower().Matches(RefType.Lower().Append("*")) && attachment.REFID == RefId)
+            attachments.push_back(attachment);
+    }
+    return attachments;
 }
 
 /** Return the number of attachments linked to a specific object */
 int Model_Attachment::NrAttachments(const wxString& RefType, const int RefId)
 {
-	return Model_Attachment::instance().find(Model_Attachment::DB_Table_ATTACHMENT_V1::REFTYPE(RefType), Model_Attachment::REFID(RefId)).size();
+    return Model_Attachment::instance().find(Model_Attachment::DB_Table_ATTACHMENT_V1::REFTYPE(RefType), Model_Attachment::REFID(RefId)).size();
 }
 
 /** Return the last attachment number linked to a specific object */
@@ -83,12 +83,12 @@ int Model_Attachment::LastAttachmentNumber(const wxString& RefType, const int Re
     Model_Attachment::Data_Set attachments = Model_Attachment::instance().FilterAttachments(RefType, RefId);
 
     for (auto &attachment : attachments)
-	{
+    {
         wxString FileName = attachment.FILENAME;
         int AttachNumb = wxAtoi(FileName.SubString(FileName.Find("Attach") + 6, FileName.Find(".") - 1));
         if (AttachNumb > LastAttachmentNumber)
             LastAttachmentNumber = AttachNumb;
-	}
+    }
 
     return LastAttachmentNumber;
 }
@@ -96,9 +96,9 @@ int Model_Attachment::LastAttachmentNumber(const wxString& RefType, const int Re
 /** Return the description of the choice reftype */
 wxString Model_Attachment::reftype_desc(const int RefTypeEnum)
 {
-	const auto& item = REFTYPE_CHOICES[RefTypeEnum];
-	wxString reftype_desc = item.second;
-	return reftype_desc;
+    const auto& item = REFTYPE_CHOICES[RefTypeEnum];
+    wxString reftype_desc = item.second;
+    return reftype_desc;
 }
 
 /** Return a dataset with attachments linked to a specific type*/
