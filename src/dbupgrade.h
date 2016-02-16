fe0c741f4c3e96c8c5bf6964b9d9f39582ecbdf3
Gabriele-V <gabriele@villanet.it>
2016-02-16 08:03:50 +0100

    feat(db.upgrade): fix path

--- src/dbupgrade.h
+++ src/dbupgrade.h
@@ -20,7 +20,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #define MM_EX_DBUPGRADE_H_
 
 #include "dbupgrade_query.h"
-#include "db\DB_Table.h"
+#include "db/DB_Table.h"
 
 class dbUpgrade
 {
