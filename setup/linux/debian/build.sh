cbb0f442d040e106c6f0d507b28dc94a6dde893e
vomikan <vomikan@mail.ru>
2016-03-15 10:28:36 +0300

    fix(version): update to 1.2.7

--- setup/linux/debian/build.sh
+++ setup/linux/debian/build.sh
@@ -13,7 +13,7 @@
 ARCHITECTURE="amd64"
 
 # Specify the build version of mmex
-MMEX_VERSION="1.2.5"
+MMEX_VERSION="1.2.7"
 MMEX_RELEASE_DATE="2016-01-24"
 EMAIL="moneymanagerex@moneymanagerex.org"
 HOMEPAGE="http://www.moneymanagerex.org"
