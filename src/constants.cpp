e67f0bef5c2b634731ccb3a79ddfce67d18b27c0
Lisheng Guan <guanlisheng@gmail.com>
2016-06-08 01:20:49 +0800

    fix(winsock): try to fix the conflict due to winsock and winsock2

--- src/constants.cpp
+++ src/constants.cpp
@@ -16,6 +16,10 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  ********************************************************/
 
+#ifdef __WXMSW__
+#define WIN32_LEAN_AND_MEAN
+#endif
+
 #include "constants.h"
 #include <wx/string.h>
 #include <wx/filefn.h>
