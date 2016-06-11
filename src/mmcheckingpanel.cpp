0fd53fff2f006105ff35e838f71d5508e3ff75f5
Stefano <stef145g@gmail.com>
2016-06-12 00:10:26 +1000

    fix(Popup.Menu.Items): foreign transactions

--- src/mmcheckingpanel.cpp
+++ src/mmcheckingpanel.cpp
@@ -1124,7 +1124,7 @@ void TransactionListCtrl::OnMouseRightClick(wxMouseEvent& event)
     menu.Append(MENU_SUBMENU_MARK_ALL, _("Mark all being viewed"), subGlobalOpMenu);
 
     // Disable menu items not ment for foreign transactions
-    if (!is_foreign)
+    if (is_foreign)
     {
         menu.Enable(MENU_ON_COPY_TRANSACTION, false);
         menu.Enable(MENU_ON_PASTE_TRANSACTION, false);
