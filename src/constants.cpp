f50166d04bfc6584a9d0bccfe68f0214c029516f
guanlisheng <guanlisheng@gmail.com>
2016-08-13 20:46:33 +0800

    feat(release): 1.3.0-beta.4

--- src/constants.cpp
+++ src/constants.cpp
@@ -45,7 +45,7 @@ const int mmex::version::Major = 1;
 const int mmex::version::Minor = 3;
 const int mmex::version::Patch = 0;
 const int mmex::version::Alpha = -1;
-const int mmex::version::Beta  = 2;
+const int mmex::version::Beta  = 4;
 const int mmex::version::RC    = -1;
 const wxString mmex::version::string = mmex::version::generateProgramVersion(mmex::version::Major, mmex::version::Minor, mmex::version::Patch
     ,mmex::version::Alpha, mmex::version::Beta, mmex::version::RC);
