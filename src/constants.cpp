4ceb882e421db214547bc07294f383ffa466c449
Lisheng <guanlisheng@gmail.com>
2016-12-08 22:49:43 +0800

    v1.3.0 GA

--- src/constants.cpp
+++ src/constants.cpp
@@ -46,7 +46,7 @@ const int mmex::version::Minor = 3;
 const int mmex::version::Patch = 0;
 const int mmex::version::Alpha = -1;
 const int mmex::version::Beta  = -1;
-const int mmex::version::RC    = 2;
+const int mmex::version::RC    = -1;
 const wxString mmex::version::string = mmex::version::generateProgramVersion(mmex::version::Major, mmex::version::Minor, mmex::version::Patch
     ,mmex::version::Alpha, mmex::version::Beta, mmex::version::RC);
 
