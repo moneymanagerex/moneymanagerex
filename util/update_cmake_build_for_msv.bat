d230a85d496ae93f4fe4e768b78b2bbfb65ff9e7
vomikan <vomikan@mail.ru>
2017-04-29 12:29:44 +0300

    fix(msvc.cmake): update locations for support files

--- util/update_cmake_build_for_msv.bat
+++ util/update_cmake_build_for_msv.bat
@@ -4,76 +4,88 @@ set log=%temp%\update_cmake_build_for_msv.log
 REM --------------------------------------------------------------------------
 REM Author : Stefano Giorgio [stef145g] - Copyright (C) 2017
 REM 
-REM Purpose: Collection of MMEX support files in CMake build locations.
-REM          Allows MMEX to be run successfilly within MSVC 2015 IDE
+REM Purpose: To collect all the MMEX support files required to run MMEX.EXE
+REM          within MS Visual Studio IDE on completion of an MSVC build.
 REM --------------------------------------------------------------------------
+
+REM Current Build Types: Debug, Release, MinSizeRel
+set cmake_build_location=..\bin
+set current_build_type=Debug
 
 REM --------------------------------------------------------------------------  
 REM This will collect the required files for existing build locations.
 REM --------------------------------------------------------------------------  
-
-set cmake_build_location=..\build
-
-REM Current Build Types: Debug, Release
-for %%x in (Release Debug) do (
-if not exist %cmake_build_location%\%%x mkdir %cmake_build_location%\%%x 
+:Update_Files
+set mmex_build_dir=%cmake_build_location%\%current_build_type%
+if not exist %mmex_build_dir% goto Next_Update
+@echo ======================================================================== >>%log%
+@echo Collect MMEX Support files for: %mmex_build_dir% >>%log%
+@echo ======================================================================== >>%log%
 @echo. >>%log%
+@echo Copy Root files for: %current_build_type% >>%log%
+@echo ------------------------------------------------------------------------ >>%log%
+copy "..\doc\*.txt" %mmex_build_dir% >>%log%
+copy "..\readme.*"  %mmex_build_dir% >>%log%
+@echo. >>%log%
+@echo ------------------------------------------------------------------------ >>%log%
+@echo Updating Help files for: %current_build_type% to: %mmex_build_dir%\help >>%log%
+@echo ------------------------------------------------------------------------ >>%log%
+if not exist %mmex_build_dir%\help mkdir %mmex_build_dir%\help
+xcopy /Y /I /S "..\docs\*.*"            "%mmex_build_dir%\help" >>%log%
+@echo. >>%log%
+@echo ------------------------------------------------------------------------ >>%log%
+@echo Copying Language files for: %current_build_type% to: "%mmex_build_dir%\po\en" >>%log%
+@echo ------------------------------------------------------------------------ >>%log%
+if not exist %mmex_build_dir%\po mkdir %mmex_build_dir%\po
+if not exist %mmex_build_dir%\po\en mkdir %mmex_build_dir%\po\en
+copy "..\po\*.mo" "%mmex_build_dir%\po\en" >>%log%
+rem copy ".\po\*.po" "%mmex_build_dir%\po"
+@echo. >>%log%
+@echo ------------------------------------------------------------------------ >>%log%
+@echo Copying Resources files for: %current_build_type% to: "%mmex_build_dir%\res" >>%log%
+@echo ------------------------------------------------------------------------ >>%log%
+if not exist %mmex_build_dir%\res mkdir %mmex_build_dir%\res
+@echo copy "..\resources\kaching.wav"   "%mmex_build_dir%\res" >>%log%
+copy "..\resources\kaching.wav"         "%mmex_build_dir%\res" >>%log%
+@echo copy "..\resources\mmex.ico"      "%mmex_build_dir%\res" >>%log%
+copy "..\resources\mmex.ico"            "%mmex_build_dir%\res" >>%log%
+@echo copy "..\resources\master.css"    "%mmex_build_dir%\res" >>%log%
+copy "..\resources\master.css"          "%mmex_build_dir%\res" >>%log%
+@echo copy "..\resources\home_page.htt" "%mmex_build_dir%\res" >>%log%
+copy "..\resources\home_page.htt"       "%mmex_build_dir%\res" >>%log%
+@echo copy "..\3rd\ChartNew.js\ChartNew.js"      "%mmex_build_dir%\res" >>%log%
+copy "..\3rd\ChartNew.js\ChartNew.js"            "%mmex_build_dir%\res" >>%log%
+@echo copy "..\3rd\sorttable.js\sorttable.js"    "%mmex_build_dir%\res" >>%log%
+copy "..\3rd\sorttable.js\sorttable.js"          "%mmex_build_dir%\res" >>%log%
+@echo ------------------------------------------------------------------------ >>%log%
+@echo Completed for: %current_build_type% >>%log% 
 @echo ======================================================================= >>%log%
 @echo. >>%log%
-@echo ------------------------------------------------------------------------ >>%log%
-@echo Collect MMEX Support files for: %cmake_build_location%\%%x >>%log%
-@echo ------------------------------------------------------------------------ >>%log%
-@echo Copy Root files for: %%x >>%log%
-for /r %%f in (..\*.txt ..\README.*) do (
-  @echo Copy %%f to %cmake_build_location%\%%x\ >>%log%
-  copy %%f %cmake_build_location%\%%x >NUL
-) 
-
-rem copy "..\doc\*.txt" %cmake_build_location%\%%x>NUL
-rem copy "..\readme.*"  %cmake_build_location%\%%x>NUL
 @echo. >>%log%
-@echo ------------------------------------------------------------------------ >>%log%
-@echo Updating Help files for: %%x >>%log%
-@echo ------------------------------------------------------------------------ >>%log%
-(if not exist %cmake_build_location%\%%x\help mkdir %cmake_build_location%\%%x\help)
-echo Copy "..\docs\*.*" to "%cmake_build_location%\%%x\help\" >>%log%
-xcopy /Y /I /S "..\docs\*.*" "%cmake_build_location%\%%x\help\" >>%log%
 @echo. >>%log%
-@echo ------------------------------------------------------------------------ >>%log%
-@echo Copying Language files for: %%x >>%log%
-@echo ------------------------------------------------------------------------ >>%log%
-if not exist %cmake_build_location%\%%x\po mkdir %cmake_build_location%\%%x\po
-if not exist %cmake_build_location%\%%x\po\en mkdir %cmake_build_location%\%%x\po\en
-@echo Copy ".\po\*.mo" to "%cmake_build_location%\%%x\po\en\" >>%log%
-copy "..\po\*.mo" "%cmake_build_location%\%%x\po\en" >>%log%
-rem copy ".\po\*.po" "%cmake_build_location%\%%x\po"
 
+REM Runtime files for x64 - MSCV_2015 - Not needed in a build environment
+rem copy "C:\Windows\system32\msvcp140.dll"     %mmex_build_dir%
+rem copy "C:\Windows\system32\vcruntime140.dll" %mmex_build_dir%
+
+REM Work out what to do next.
+:Next_Update
+REM Continue from already processed, Debug
+if %current_build_type%==Debug      goto Update_Release
+if %current_build_type%==Release    goto Update_MinSizeRel
+goto Script_End
+
+:Update_Release
+set current_build_type=Release
+goto Update_Files
+
+:Update_MinSizeRel
+set current_build_type=MinSizeRel
+goto Update_Files
+
+:Script_End
 @echo. >>%log%
-@echo ------------------------------------------------------------------------ >>%log%
-@echo Copying Resources files for: %%x >>%log%
-@echo ------------------------------------------------------------------------ >>%log%
-if not exist %cmake_build_location%\%%x\res mkdir %cmake_build_location%\%%x\res
-for %%f in (.\resources\kaching.wav .\resources\mmex.ico .\resources\master.css .\resources\home_page.htt .\3rd\ChartNew.js\ChartNew.js .\3rd\sorttable.js\sorttable.js) DO (
-@echo Copy "%%f" to "%cmake_build_location%\%%x\res\" >>%log%
-copy "%%f"  "%cmake_build_location%\%%x\res" >NUL 
-)
-
-@echo. >>%log%
-@echo ------------------------------------------------------------------------ >>%log%
-@echo Creating CMake batch file for: %%x >>%log%
-@echo ------------------------------------------------------------------------ >>%log%
-@echo cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=%%x ../../ > %cmake_build_location%\%%x\cmake_init.bat
-@echo cmake -G "NMake Makefiles" -DCMAKE_BUILD_TYPE=%%x ../../ >>%log%
-@echo Done >>%log%
-@echo ------------------------------------------------------------------------ >>%log%
-
-REM Runtime files for x64 - MSCV_2015
-rem copy "C:\Windows\system32\msvcp140.dll"     %cmake_build_location%\%%x
-rem copy "C:\Windows\system32\vcruntime140.dll" %cmake_build_location%\%%x
-)
-
-@echo. >>%log%
-@echo Update completed. >>%log%
+@echo Total update: Completed. >>%log%
 @echo ======================================================================= >>%log%
 notepad %log%
 del %log%
