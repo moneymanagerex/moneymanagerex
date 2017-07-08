3489f23dc39422a4a7501a515382715ca068bab1
Tomasz Słodkowicz <slodki@users.noreply.github.com>
2017-07-08 14:08:21 +0200

    perf(gettext.script): optimize
    use only one find loop and not use unnecessary xargs
    do not remove locations to easy identify context

--- update-po-files.sh
+++ update-po-files.sh
@@ -1,21 +1,28 @@
 #!/bin/sh -e
 
-[ -n "${WXRC}" ] || WXRC=wxrc
+XGETTEXT_ARGS="-k_ -kwxGetTranslation -kwxTRANSLATE -kwxPLURAL:1,2 \
+               --language=C++ \
+               --sort-by-file \
+               --add-comments=TRANSLATORS \
+               --from-code=UTF-8 \
+               --add-location \
+               --package-name=MMEX \
+               --width=78 \
+               --msgid-bugs-address=developer@moneymanagerex.org"
+MSGMERGE_ARGS="--quiet \
+               --update \
+               --sort-by-file \
+               --add-location \
+               --width=78 \
+               --backup=none"
+POT=MoneyManagerEx.pot
 
-XGETTEXT_ARGS="-C -k_ -kwxGetTranslation -kwxTRANSLATE -kwxPLURAL:1,2 -F \
-              --add-comments=TRANSLATORS \
-              --from-code=UTF-8 \
-              --no-location \
-              --package-name=MMEX \
-              --msgid-bugs-address=developer@moneymanagerex.org"
-
-echo "Extracting strings into ./po/MoneyManagerEx.pot..."
-find ./src -name "*.cpp" | xargs xgettext ${XGETTEXT_ARGS}             -o ./po/MoneyManagerEx.pot
-find ./src -name "*.h"   | xargs xgettext ${XGETTEXT_ARGS}          -j -o ./po/MoneyManagerEx.pot
+echo "Extracting strings into ./po/$POT..."
+find ./src -name \*.cpp -o -name \*.h | xgettext -f - $XGETTEXT_ARGS -o ./po/$POT
 
 echo "Merging into *.po..."
 for p in ./po/*.po ; do
     echo "Merging $p ..."
-    msgmerge --quiet --update --sort-output --backup=none $p ./po/MoneyManagerEx.pot
+    msgmerge $MSGMERGE_ARGS $p ./po/$POT
 done
 echo "Finished"
