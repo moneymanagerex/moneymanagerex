6f1b4d61580622a5053106da6d44ae0a20ba93b6
Stefano <stef145g@gmail.com>
2018-01-25 11:41:09 +1100

    feat(rapidjson): conversion of option - causes crash of calling function using cajun:json - new code commented out

--- src/option.cpp
+++ src/option.cpp
@@ -683,6 +683,29 @@ mmPrintableBase* Option::ReportFunction(int report)
 
 wxString Option::ReportSettings(int id)
 {
+    //wxString name = wxString::Format("REPORT_%d", id);
+    //wxString settings = Model_Infotable::instance().GetStringInfo(name, "");
+    //if (!(settings.StartsWith("{") && settings.EndsWith("}")))
+    //{
+    //    settings = "{}";
+    //}
+
+    //Document j_doc_main;
+    //j_doc_main.SetObject();
+    //Document::AllocatorType& j_doc_main_allocator = j_doc_main.GetAllocator();
+ 
+    //if (!name.empty())
+    //{
+    //    Value j_name(name, j_doc_main_allocator);
+    //    j_doc_main.AddMember("SETTINGSNAME", j_name, j_doc_main_allocator);
+    //}
+
+    //Document j_doc_sub(&j_doc_main.GetAllocator());
+    //j_doc_sub.Parse(settings);
+    //j_doc_main.AddMember("SETTINGSDATA", j_doc_sub, j_doc_main_allocator);
+
+    //return JSON_PrettyFormated(j_doc_main);
+
     wxString name = wxString::Format("REPORT_%d", id);
     wxString settings = Model_Infotable::instance().GetStringInfo(name, "");
     if (!(settings.StartsWith("{") && settings.EndsWith("}")))
