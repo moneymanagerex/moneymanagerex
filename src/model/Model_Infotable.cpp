72613ffb7912657172ac2a56e98d1ec86f11dde9
Tomasz Słodkowicz <slodki@users.noreply.github.com>
2018-04-19 21:49:51 +0200

    fix(#1569): fix typo thx @vomikan

--- src/model/Model_Infotable.cpp
+++ src/model/Model_Infotable.cpp
@@ -70,7 +70,7 @@ void Model_Infotable::Set(const wxString& key, const wxDateTime& date)
 
 void Model_Infotable::Set(const wxString& key, const wxString& value)
 {
-    if (this->db_) return;
+    if (!this->db_) return;
 
     Data* info = this->get_one(INFONAME(key));
     if (!info) // not cached
