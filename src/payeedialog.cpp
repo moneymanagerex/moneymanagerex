d28ddfab06464b861b9caad7ae63fe56b1700836
Tomasz Słodkowicz <slodki@users.noreply.github.com>
2018-06-04 15:50:40 +0200

    fix(payeedialog): disable relocate context menu for unused payees

--- src/payeedialog.cpp
+++ src/payeedialog.cpp
@@ -379,7 +379,9 @@ void mmPayeeDialog::OnItemRightClick(wxDataViewEvent& event)
 
     mainMenu->Append(new wxMenuItem(mainMenu, MENU_RELOCATE_PAYEE, _("Relocate Payee")));
     //SetToolTip(_("Change all transactions using one Payee to another Payee"));
-    if (!payee) mainMenu->Enable(MENU_RELOCATE_PAYEE, false);
+    if (!payee || payeeListBox_->GetItemCount() < 2
+        || !Model_Payee::is_used(m_payee_id))
+            mainMenu->Enable(MENU_RELOCATE_PAYEE, false);
 
     PopupMenu(mainMenu);
     delete mainMenu;
