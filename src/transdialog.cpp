moneymanagerex/moneymanagerex

commit 9fc3f02f1e77b08d2f5099a5b83e70f632a02175

feat(BitCoin.Transaction): issue #866 (#869)

src/transdialog.cpp
@@ -180,13 +180,15 @@ void mmTransDialog::dataToControls()
             if (!m_advanced)
                 m_trx_data.TOTRANSAMOUNT = m_trx_data.TRANSAMOUNT 
                     * (m_to_currency ? m_currency->BASECONVRATE / m_to_currency->BASECONVRATE : 1);
-            toTextAmount_->SetValue(m_trx_data.TOTRANSAMOUNT);
+            toTextAmount_->SetValue(m_trx_data.TOTRANSAMOUNT, Model_Currency::precision(m_trx_data.TOACCOUNTID));
         }
         else
             toTextAmount_->ChangeValue("");
 
         if (m_trx_data.TRANSID != -1)
-            textAmount_->SetValue(m_trx_data.TRANSAMOUNT);
+        {
+            m_textAmount->SetValue(m_trx_data.TRANSAMOUNT, Model_Currency::precision(m_trx_data.ACCOUNTID));
+        }
         skip_amount_init_ = true;
     }
 
@@ -314,7 +316,7 @@ void mmTransDialog::dataToControls()
         if (has_split)
         {
             fullCategoryName = _("Categories");
-            textAmount_->SetValue(Model_Splittransaction::get_total(local_splits));
+            m_textAmount->SetValue(Model_Splittransaction::get_total(local_splits));
             m_trx_data.CATEGID = -1;
             m_trx_data.SUBCATEGID = -1;
         }
@@ -330,7 +332,7 @@ void mmTransDialog::dataToControls()
         cSplit_->SetValue(has_split);
         skip_category_init_ = true;
     }
-    textAmount_->Enable(local_splits.empty());
+    m_textAmount->Enable(local_splits.empty());
     cSplit_->Enable(!m_transfer);
 
     if (!skip_notes_init_) //Notes & Transaction Number
@@ -411,7 +413,7 @@ void mmTransDialog::CreateControls()
     typeSizer->Add(cAdvanced_, g_flagsH);
 
     // Amount Fields --------------------------------------------
-    textAmount_ = new mmTextCtrl(this, ID_DIALOG_TRANS_TEXTAMOUNT, ""
+    m_textAmount = new mmTextCtrl(this, ID_DIALOG_TRANS_TEXTAMOUNT, ""
         , wxDefaultPosition, wxSize(110, -1)
         , wxALIGN_RIGHT | wxTE_PROCESS_ENTER, mmCalcValidator());
 
@@ -420,7 +422,7 @@ void mmTransDialog::CreateControls()
         , wxALIGN_RIGHT | wxTE_PROCESS_ENTER, mmCalcValidator());
 
     wxBoxSizer* amountSizer = new wxBoxSizer(wxHORIZONTAL);
-    amountSizer->Add(textAmount_, g_flagsH);
+    amountSizer->Add(m_textAmount, g_flagsH);
     amountSizer->Add(toTextAmount_, g_flagsH);
 
     flex_sizer->Add(new wxStaticText( this, wxID_STATIC, _("Amount")), g_flagsH);
@@ -531,7 +533,7 @@ void mmTransDialog::CreateControls()
 
     cbPayee_->Connect(ID_DIALOG_TRANS_PAYEECOMBO, wxEVT_COMMAND_TEXT_UPDATED
         , wxCommandEventHandler(mmTransDialog::OnAccountOrPayeeUpdated), nullptr, this);
-    textAmount_->Connect(ID_DIALOG_TRANS_TEXTAMOUNT, wxEVT_COMMAND_TEXT_ENTER
+    m_textAmount->Connect(ID_DIALOG_TRANS_TEXTAMOUNT, wxEVT_COMMAND_TEXT_ENTER
         , wxCommandEventHandler(mmTransDialog::onTextEntered), nullptr, this);
     toTextAmount_->Connect(ID_DIALOG_TRANS_TOTEXTAMOUNT, wxEVT_COMMAND_TEXT_ENTER
         , wxCommandEventHandler(mmTransDialog::onTextEntered), nullptr, this);
@@ -547,7 +549,7 @@ void mmTransDialog::CreateControls()
 
 bool mmTransDialog::validateData()
 {
-    if (!textAmount_->checkValue(m_trx_data.TRANSAMOUNT))
+    if (!m_textAmount->checkValue(m_trx_data.TRANSAMOUNT))
         return false;
 
     Model_Account::Data* account = Model_Account::instance().get(cbAccount_->GetValue());
@@ -698,15 +700,15 @@ void mmTransDialog::onFocusChange(wxChildFocusEvent& event)
         }
     }
 
-    if (object_in_focus_ == textAmount_->GetId())
+    if (object_in_focus_ == m_textAmount->GetId())
     {
-        textAmount_->SelectAll();
+        m_textAmount->SelectAll();
     }
     else 
     {
-        if (textAmount_->Calculate())
+        if (m_textAmount->Calculate(Model_Currency::precision(m_trx_data.ACCOUNTID)))
         {
-            textAmount_->GetDouble(m_trx_data.TRANSAMOUNT);
+            m_textAmount->GetDouble(m_trx_data.TRANSAMOUNT);
         }
         skip_amount_init_ = false;
     }
@@ -717,8 +719,10 @@ void mmTransDialog::onFocusChange(wxChildFocusEvent& event)
     }
     else 
     {
-        if (toTextAmount_->Calculate())
+        if (toTextAmount_->Calculate(Model_Currency::precision(m_trx_data.TOACCOUNTID)))
+        {
             toTextAmount_->GetDouble(m_trx_data.TOTRANSAMOUNT);
+        }
     }
 
     dataToControls();
@@ -729,7 +733,7 @@ void mmTransDialog::activateSplitTransactionsDlg()
 {
     bool bDeposit = Model_Checking::is_deposit(m_trx_data.TRANSCODE);
 
-    if (!textAmount_->GetDouble(m_trx_data.TRANSAMOUNT))
+    if (!m_textAmount->GetDouble(m_trx_data.TRANSAMOUNT))
         m_trx_data.TRANSAMOUNT = 0;
 
     if (!Model_Category::full_name(m_trx_data.CATEGID, m_trx_data.SUBCATEGID).empty() && local_splits.empty())
@@ -967,18 +971,23 @@ void mmTransDialog::OnCustomFields(wxCommandEvent& /*event*/)
 
 void mmTransDialog::onTextEntered(wxCommandEvent& WXUNUSED(event))
 {
-    if (object_in_focus_ == textAmount_->GetId()) 
+    if (object_in_focus_ == m_textAmount->GetId())
     {
-        if (textAmount_->Calculate())
-            textAmount_->GetDouble(m_trx_data.TRANSAMOUNT);
+        if (m_textAmount->Calculate(Model_Currency::precision(m_currency)))
+        {
+            m_textAmount->GetDouble(m_trx_data.TRANSAMOUNT);
+        }
         skip_amount_init_ = false;
         dataToControls();
     }
-    else if (object_in_focus_ == toTextAmount_->GetId())
-        toTextAmount_->Calculate();
+    else if (object_in_focus_ == m_textAmount->GetId())
+    {
+        m_textAmount->Calculate(Model_Currency::precision(m_currency));
+    }
     else if (object_in_focus_ == textNumber_->GetId())
-        textNotes_->SetFocus();
-
+    {
+        m_textAmount->SetFocus();
+    }
 }
 
 void mmTransDialog::OnFrequentUsedNotes(wxCommandEvent& WXUNUSED(event))
@@ -1138,7 +1147,7 @@ void mmTransDialog::setTooltips()
     }
     if (!m_new_trx) return;
 
-    textAmount_->UnsetToolTip();
+    m_textAmount->UnsetToolTip();
     toTextAmount_->UnsetToolTip();
     cbAccount_->UnsetToolTip();
     cbPayee_->UnsetToolTip();
@@ -1147,14 +1156,14 @@ void mmTransDialog::setTooltips()
     {
         cbAccount_->SetToolTip(_("Specify account the money is taken from"));
         cbPayee_->SetToolTip(_("Specify account the money is moved to"));
-        textAmount_->SetToolTip(_("Specify the transfer amount in the From Account."));
+        m_textAmount->SetToolTip(_("Specify the transfer amount in the From Account."));
 
         if (m_advanced)
             toTextAmount_->SetToolTip(_("Specify the transfer amount in the To Account"));
     }
     else
     {
-        textAmount_->SetToolTip(_("Specify the amount for this transaction"));
+        m_textAmount->SetToolTip(_("Specify the amount for this transaction"));
         cbAccount_->SetToolTip(_("Specify account for the transaction"));
         if (!Model_Checking::is_deposit(m_trx_data.TRANSCODE))
             cbPayee_->SetToolTip(_("Specify to whom the transaction is going to"));

src/accountdialog.cpp
@@ -69,7 +69,7 @@ mmNewAcctDialog::mmNewAcctDialog(Model_Account::Data* account, wxWindow* parent,
     , m_currencyID(-1)
     , m_textAccountName(nullptr)
     , m_notesCtrl(nullptr)
-    , m_itemInitValue(nullptr)
+    , m_initbalance_ctrl(nullptr)
     , m_imageList(nullptr)
     , m_bitmapButtons(nullptr)
     , m_statement_lock_ctrl(nullptr)
@@ -154,7 +154,7 @@ void mmNewAcctDialog::fillControls()
     m_currencyID = m_account->CURRENCYID;
 
     double initBal = m_account->INITIALBAL;
-    m_itemInitValue->SetValue(Model_Currency::toString(initBal, Model_Account::currency(m_account)));
+    m_initbalance_ctrl->SetValue(Model_Currency::toString(initBal, Model_Account::currency(m_account)));
 
     int selectedImage = Option::instance().AccountImageId(m_account->ACCOUNTID);
     m_bitmapButtons->SetBitmap(m_imageList->GetBitmap(selectedImage));
@@ -211,10 +211,10 @@ void mmNewAcctDialog::CreateControls()
     grid_sizer->Add(new wxStaticText(this, wxID_STATIC
         , wxString::Format(_("Initial Balance: %s"), "")), g_flagsH);
 
-    m_itemInitValue = new mmTextCtrl(this
+    m_initbalance_ctrl = new mmTextCtrl(this
         , ID_DIALOG_NEWACCT_TEXTCTRL_INITBALANCE
         , "", wxDefaultPosition, wxDefaultSize, wxTE_PROCESS_ENTER, mmCalcValidator());
-    grid_sizer->Add(m_itemInitValue, g_flagsExpand);
+    grid_sizer->Add(m_initbalance_ctrl, g_flagsExpand);
 
     grid_sizer->Add(new wxStaticText(this, wxID_STATIC, _("Currency:")), g_flagsH);
 
@@ -374,7 +374,7 @@ void mmNewAcctDialog::CreateControls()
         m_textAccountName->SetToolTip(_("Enter the Name of the Account. This name can be renamed at any time."));
         itemChoice61->SetToolTip(_("Specify the type of account to be created."));
         itemChoice6->SetToolTip(_("Specify if this account has been closed. Closed accounts are inactive in most calculations, reporting etc."));
-        m_itemInitValue->SetToolTip(_("Enter the initial balance in this account."));
+        m_initbalance_ctrl->SetToolTip(_("Enter the initial balance in this account."));
         itemButton71->SetToolTip(_("Specify the currency to be used by this account."));
         itemCheckBox10->SetToolTip(_("Select whether this is an account that is used often. This is used to filter accounts display view."));
         m_notesCtrl->SetToolTip(_("Enter user notes and details about this account."));
@@ -404,7 +404,16 @@ void mmNewAcctDialog::OnCurrency(wxCommandEvent& /*event*/)
         wxButton* bn = (wxButton*)FindWindow(ID_DIALOG_NEWACCT_BUTTON_CURRENCY);
         bn->SetLabelText(currency->CURRENCYNAME);
 
-        if (this->m_account) m_account->CURRENCYID = currency->CURRENCYID;
+        double init_balance;
+        if (m_initbalance_ctrl->checkValue(init_balance, false))
+        {
+            m_initbalance_ctrl->SetValue(Model_Currency::toString(init_balance, currency));
+        }
+
+        if (this->m_account)
+        {
+            m_account->CURRENCYID = currency->CURRENCYID;
+        }
     }
 }
 
@@ -427,8 +436,9 @@ void mmNewAcctDialog::OnOk(wxCommandEvent& /*event*/)
     if (m_currencyID == -1)
         return mmErrorDialogs::MessageInvalid(this, _("Currency"));
 
-    m_itemInitValue->Calculate();
-    if (!m_itemInitValue->checkValue(m_account->INITIALBAL, false))
+    Model_Currency::Data* currency = Model_Currency::instance().get(m_currencyID);
+    m_initbalance_ctrl->Calculate(Model_Currency::precision(currency));
+    if (!m_initbalance_ctrl->checkValue(m_account->INITIALBAL, false))
         return;
 
     if (!this->m_account) this->m_account = Model_Account::instance().create();
@@ -516,11 +526,13 @@ void mmNewAcctDialog::OnCustonImage(wxCommandEvent& event)
 
 void mmNewAcctDialog::OnTextEntered(wxCommandEvent& event)
 {
-    if (event.GetId() == m_itemInitValue->GetId())
+    if (event.GetId() == m_initbalance_ctrl->GetId())
     {
         Model_Currency::Data* currency = Model_Currency::instance().get(m_currencyID);
         if (!currency)
+        {
             currency = Model_Currency::GetBaseCurrency();
-        m_itemInitValue->Calculate();
+        }
+        m_initbalance_ctrl->Calculate(Model_Currency::precision(currency));
     }
 }

src/accountdialog.h
@@ -58,7 +58,7 @@ private:
     Model_Account::Data* m_account;
     wxTextCtrl* m_textAccountName;
     wxTextCtrl* m_notesCtrl;
-    mmTextCtrl* m_itemInitValue;
+    mmTextCtrl* m_initbalance_ctrl;
     wxImageList* m_imageList;
     wxBitmapButton* m_bitmapButtons;
     wxBitmapButton* bAttachments_;

src/billsdepositsdialog.h
@@ -77,10 +77,10 @@ private:
     void OnTextEntered(wxCommandEvent& event);
     int transID_;
 
-    wxTextCtrl *textNumber_;
-    mmTextCtrl *textAmount_;
-    mmTextCtrl *toTextAmount_;
-    wxTextCtrl *textNotes_;
+    wxTextCtrl* textNumber_;
+    mmTextCtrl* textAmount_;
+    mmTextCtrl* toTextAmount_;
+    wxTextCtrl* textNotes_;
     wxTextCtrl* textCategory_;
     wxTextCtrl* textNumRepeats_;
     wxButton* bCategory_;
@@ -91,8 +91,8 @@ private:
     wxCheckBox* cAdvanced_;
     bool payeeUnknown_;
 
-    wxChoice* choiceStatus_;
-    wxChoice* transaction_type_;
+    wxChoice* m_choice_status;
+    wxChoice* m_choice_transaction_type;
 
     bool m_new_bill;
     bool m_transfer;
@@ -109,7 +109,6 @@ private:
     bool autoExecuteSilent_;
 
     Model_Billsdeposits::Bill_Data m_bill_data;
-    std::vector<Split> local_splits;
 
     bool m_advanced;
     bool categUpdated_;
@@ -119,8 +118,8 @@ private:
     wxString payeeWithdrawalTip_;
     wxString amountNormalTip_;
     wxString amountTransferTip_;
-    wxSpinButton *spinNextOccDate_;
-    wxSpinButton *spinTransDate_;
+    wxSpinButton* spinNextOccDate_;
+    wxSpinButton* spinTransDate_;
 
     void setToolTipsForType(Model_Billsdeposits::TYPE transType, bool enableAdvanced = false);
     void resetPayeeString();
@@ -140,7 +139,7 @@ private:
 
     wxStaticText* staticTimesRepeat_;
     wxStaticText* staticTextRepeats_;
-    wxButton* bSetNextOccurDate_;
+    wxButton* m_btn_due_date;
     void OnRepeatTypeChanged(wxCommandEvent& event);
     void OnsetNextRepeatDate(wxCommandEvent& event);
     void OnPeriodChange(wxCommandEvent& event);

src/currencydialog.cpp
@@ -39,7 +39,7 @@ EVT_TEXT(ID_DIALOG_CURRENCY, mmCurrencyDialog::OnTextChanged)
 EVT_TEXT_ENTER(ID_DIALOG_CURRENCY_RATE, mmCurrencyDialog::OnTextEntered)
 wxEND_EVENT_TABLE()
 
-static const int SCALE = 6;
+static const int SCALE = 9;
 
 mmCurrencyDialog::mmCurrencyDialog()
 {

src/maincurrencydialog.cpp
@@ -44,7 +44,7 @@ wxBEGIN_EVENT_TABLE(mmMainCurrencyDialog, wxDialog)
     EVT_BUTTON(wxID_REMOVE, mmMainCurrencyDialog::OnBtnDelete)
     EVT_BUTTON(wxID_CANCEL, mmMainCurrencyDialog::OnCancel)
 
-    EVT_MENU_RANGE(MENU_ITEM1, MENU_ITEM2, mmMainCurrencyDialog::OnMenuSelected)
+    EVT_MENU_RANGE(MENU_ITEM1, MENU_ITEM3, mmMainCurrencyDialog::OnMenuSelected)
 
     EVT_DATAVIEW_ITEM_ACTIVATED(wxID_ANY, mmMainCurrencyDialog::OnListItemActivated)
     EVT_DATAVIEW_SELECTION_CHANGED(wxID_ANY, mmMainCurrencyDialog::OnListItemSelected)
@@ -521,6 +521,11 @@ void mmMainCurrencyDialog::OnMenuSelected(wxCommandEvent& event)
             ShowCurrencyHistory();
         } break;
         case MENU_ITEM2: OnlineUpdateCurRate(currencyID_,false); break;
+        case MENU_ITEM3:
+        {
+            wxCommandEvent e;
+            this->OnBtnEdit(e);
+        } break;
     } 
 }
 
@@ -532,7 +537,8 @@ void mmMainCurrencyDialog::OnItemRightClick(wxDataViewEvent& event)
     wxMenu* mainMenu = new wxMenu;
     mainMenu->Append(new wxMenuItem(mainMenu, MENU_ITEM1, _("Set as Base Currency")));
     mainMenu->Append(new wxMenuItem(mainMenu, MENU_ITEM2, _("Online Update Currency Rate")));
-    
+    mainMenu->Append(new wxMenuItem(mainMenu, MENU_ITEM3, _("&Edit ")));
+
     int baseCurrencyID = Option::instance().BaseCurrency();
     if (baseCurrencyID == currencyID_)
         mainMenu->Enable(MENU_ITEM1, false);

src/maincurrencydialog.h
@@ -63,7 +63,8 @@ private:
         HISTORY_UPDATE,
         HISTORY_DELUNUSED,
         MENU_ITEM1,
-        MENU_ITEM2
+        MENU_ITEM2,
+        MENU_ITEM3
     };
 
     bool Create(wxWindow* parent

commit a922ab4d9a5124e928c4da07ed177f6c11f4eff3

fix(transdialog): calculate to_ammount value on Enter
broken in 9fc3f02f #869

src/transdialog.cpp
@@ -1067,9 +1067,12 @@ void mmTransDialog::OnTextEntered(wxCommandEvent& WXUNUSED(event))
         skip_amount_init_ = false;
         dataToControls();
     }
-    else if (object_in_focus_ == m_textAmount->GetId())
+    else if (object_in_focus_ == toTextAmount_->GetId())
     {
-        m_textAmount->Calculate(Model_Currency::precision(m_currency));
+        if (toTextAmount_->Calculate(Model_Currency::precision(m_trx_data.TOACCOUNTID)))
+        {
+            toTextAmount_->GetDouble(m_trx_data.TOTRANSAMOUNT);
+        }
     }
     else if (object_in_focus_ == textNumber_->GetId())
     {