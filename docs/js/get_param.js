1deb4bf55740b68dda7a5592eb59dfd7bc841e5a
Tomasz Słodkowicz <slodki@users.noreply.github.com>
2019-02-10 01:22:37 +0100

    refactor(docs): move language menu to separate js file

--- docs/js/get_param.js
+++
@@ -1,8 +0,0 @@
-function getParameterValue(parameter) {
-    parameter = parameter.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
-    var regexS = "[\\?&]" + parameter + "=([^&]*)";
-    var regex = new RegExp(regexS);
-    var results = regex.exec(window.location.search);
-    if (results == null) return null;
-    return results[1];
-}
