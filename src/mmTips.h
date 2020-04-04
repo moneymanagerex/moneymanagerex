fa5c68075176a5ab098bdea2e809ebcc292e0039
Nikolay <nikolay.akimov@gmail.com>
2020-04-04 19:42:25 +0300

    fix(v134): info update

--- src/mmTips.h
+++ src/mmTips.h
@@ -34,7 +34,7 @@ static const wxString TIPS [] =
     wxTRANSLATE("Organize Categories Dialog Tip: Pressing the h key will cycle through all categories starting with the letter h"),
     wxTRANSLATE("Organize Categories Dialog Tip: Pressing 2 key combination will cycle through all categories starting with that key combination. Example: Pressing ho will select Homeneeds, Home, House Tax, etc..."),
 
-    wxTRANSLATE("Tip to get out of debt: Pay yourself 10% first. Put this into an account that is hard to touch. Make sure it is a chore to get the money out (you have to drive to the bank), so you will only tap it consciously and for major expenses."),
+    wxTRANSLATE("Tip to get out of debt: Pay yourself 10 percentage first. Put this into an account that is hard to touch. Make sure it is a chore to get the money out (you have to drive to the bank), so you will only tap it consciously and for major expenses."),
     wxTRANSLATE("Tip to get out of debt: Establish an emergency fund."),
     wxTRANSLATE("Tip to get out of debt: Stop acquiring new debt."),
     wxTRANSLATE("Tip to get out of debt: Create a realistic budget for your expenses."),
