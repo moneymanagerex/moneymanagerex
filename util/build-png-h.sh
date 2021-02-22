38687cd72cd07a470f5d29593e803804fde86404
Mark Whalley <mark@ipx.co.uk>
2021-02-22 08:42:31 +0000

    fix(#2923):codacy warning fix

--- util/build-png-h.sh
+++ util/build-png-h.sh
@@ -8,11 +8,11 @@
 rm -f icons.h
 for file in "$@"
 do
-    filename=$(basename ${file} .svg)
+    filename="$(basename ${file} .svg)"
     for size in 16 24 32 48
     do  
-        rsvg-convert -h ${size} -f png ${file} > ${filename}_${size}.png
-        xxd -i ${filename}_${size}.png | sed '1s/^/static const /' |  sed 's/_\([0-9][0-9]\)/\1/' | awk 'NR>1{print buf}{buf = $0}' >> icons.h
-        rm ${filename}_${size}.png
+        rsvg-convert -h "${size}" -f png "${file}" > "${filename}_${size}".png
+        xxd -i "${filename}_${size}".png | sed "1s/^/static const /" |  sed "s/_\([0-9][0-9]\)/\1/" | awk "NR>1{print buf}{buf = \$0}" >> icons.h
+        rm "${filename}_${size}".png
     done
 done
