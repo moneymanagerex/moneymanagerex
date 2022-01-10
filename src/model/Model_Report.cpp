4d4538bf86ca080968af45674014980552624c5d
vomikan <nikolay.akimov@gmail.com>
2022-01-10 12:57:51 +0300

    fix(#2014): language canonical name

--- src/model/Model_Report.cpp
+++ src/model/Model_Report.cpp
@@ -384,7 +384,7 @@ int Model_Report::get_html(const Data* r, wxString& out)
         auto s = wxString(wxFileName::GetPathSeparator());
         s.Replace("\\", "\\\\");
         report(L"FILESEPARATOR") = s;
-        report(L"LANGUAGE") = Option::instance().getLanguageISO6391();
+        report(L"LANGUAGE") = Option::instance().getLanguageCode();
         report(L"HTMLSCALE") = wxString::Format("%d", Option::instance().getHtmlFontSize());
     }
     report(L"ERRORS") = errors;
