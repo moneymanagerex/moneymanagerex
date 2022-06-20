56d1503b1bd7f42369bd2f3eaf33c98996ccc100
vomikan <nikolay.akimov@gmail.com>
2022-06-20 16:03:21 +0300

    fix(#4651): precision

--- src/mmcustomdata.cpp
+++ src/mmcustomdata.cpp
@@ -160,7 +160,7 @@ bool mmCustomData::FillCustomFields(wxBoxSizer* box_sizer)
             int DigitScale = Model_CustomField::getDigitScale(field.PROPERTIES);
             wxSpinCtrlDouble* CustomDecimal = new wxSpinCtrlDouble(scrolled_window, controlID
                 , wxEmptyString, wxDefaultPosition, wxDefaultSize
-                , wxSP_ARROW_KEYS, -2147483647, 2147483647, value, 1);
+                , wxSP_ARROW_KEYS, -2147483647, 2147483647, value, 1.0 / pow10(DigitScale));
             CustomDecimal->SetDigits(DigitScale);
             mmToolTip(CustomDecimal, Model_CustomField::getTooltip(field.PROPERTIES));
             grid_sizer_custom->Add(CustomDecimal, g_flagsExpand);
