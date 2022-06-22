81c0f2a205abd5ba929f1f04528793538fc4ad90
Mark Whalley <mark@ipx.co.uk>
2022-06-22 11:09:50 +0100

    fix(#4620): revert logic change

--- src/transdialog.cpp
+++ src/transdialog.cpp
@@ -318,8 +318,7 @@ void mmTransDialog::dataToControls()
                 if (payee) cbPayee_->ChangeValue(payee->PAYEENAME);
             }
 
-            if (m_new_trx && !m_duplicate)
-                SetCategoryForPayee();
+            SetCategoryForPayee();
         }
         skip_payee_init_ = true;
         cbPayee_->SetEvtHandlerEnabled(true);
@@ -948,7 +947,7 @@ void mmTransDialog::SetCategoryForPayee(const Model_Payee::Data *payee)
     // Only for new transactions: if user does not want to use categories.
     // If this is a Split Transaction, ignore displaying last category for payee
     if (Option::instance().TransCategorySelection() == Option::UNUSED
-        && m_local_splits.empty())
+        && m_local_splits.empty() && m_new_trx && !m_duplicate)
     {
         Model_Category::Data *category = Model_Category::instance().get(_("Unknown"));
         if (!category)
@@ -975,7 +974,7 @@ void mmTransDialog::SetCategoryForPayee(const Model_Payee::Data *payee)
     // If this is a Split Transaction, ignore displaying last category for payee
     if ((Option::instance().TransCategorySelection() == Option::LASTUSED ||
          Option::instance().TransCategorySelection() == Option::DEFAULT)
-        && m_local_splits.empty())
+        && m_local_splits.empty() && m_new_trx && !m_duplicate)
     {
         // if payee has memory of last category used then display last category for payee
         Model_Category::Data *category = Model_Category::instance().get(payee->CATEGID);
