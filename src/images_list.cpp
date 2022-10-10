5b3f5acbf0b0de18e45d02c47fd72f75fb18b286
Mark Whalley <mark@ipx.co.uk>
2022-10-10 08:56:14 +0100

    fix(#3779): need to incldue <memory> for Linux

--- src/images_list.cpp
+++ src/images_list.cpp
@@ -33,6 +33,7 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 #include <wx/fs_mem.h>
 #include <wx/mstream.h>
 #include <wx/tokenzr.h>
+#include <memory>
 //#include "../3rd/lunasvg/include/lunasvg.h"
 
 #include <array>
