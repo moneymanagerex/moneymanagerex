0fd9cd9c831f55336972154f43c7100cb7ddcf9e
n-stein <16599929+n-stein@users.noreply.github.com>
2022-11-29 16:49:47 -0500

    fix(#5381): apply forex rate to asset curr value

--- src/model/Model_Asset.cpp
+++ src/model/Model_Asset.cpp
@@ -227,7 +227,7 @@ double Model_Asset::valueAtDate(const Data* r, const wxDate date)
                 if (tranDate <= date)
                 {
                     double amount = -1 * Model_Checking::balance(tran, tran->ACCOUNTID) *
-                        Model_CurrencyHistory::getDayRate(Model_Account::instance().get(tran->ACCOUNTID)->CURRENCYID, date);
+                        Model_CurrencyHistory::getDayRate(Model_Account::instance().get(tran->ACCOUNTID)->CURRENCYID, tranDate);
                     wxTimeSpan diff_time = date - tranDate;
                     double diff_time_in_days = static_cast<double>(diff_time.GetDays());
 
