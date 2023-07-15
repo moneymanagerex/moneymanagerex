1cdb9232c2a5c1ec610eefdafbe33229853211f0
ovari <17465872+ovari@users.noreply.github.com>
2023-07-15 14:56:03 +1000

    Update relocatecategorydialog.h

--- src/relocatecategorydialog.h
+++ src/relocatecategorydialog.h
@@ -40,7 +40,7 @@ public:
 
     bool Create(wxWindow* parent
         , wxWindowID id = wxID_ANY
-        , const wxString& caption = _("Relocate Category Dialog")
+        , const wxString& caption = _("Relocate Category")
         , const wxPoint& pos = wxDefaultPosition
         , const wxSize& size = wxDefaultSize
         , long style = wxCAPTION | wxSYSTEM_MENU | wxCLOSE_BOX
