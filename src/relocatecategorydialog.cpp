04c5e4de2224a5002e2f4ce63582dade9094e5f6
ovari <17465872+ovari@users.noreply.github.com>
2023-07-16 12:17:50 +1000

    Update relocatecategorydialog.cpp
    https://crowdin.com/translate/moneymanagerex/470/en-hu#798108

--- src/relocatecategorydialog.cpp
+++ src/relocatecategorydialog.cpp
@@ -101,7 +101,7 @@ void relocateCategoryDialog::CreateControls()
     cbDestCategory_->SetMinSize(wxSize(200, -1));
 
     cbDeleteSourceCategory_ = new wxCheckBox(this, wxID_ANY
-        , _("Delete source category after relocation (if it has no sub-categories)"));
+        , _("Delete source category after merge (if there are no sub-categories)"));
 
     wxStaticLine* lineBottom = new wxStaticLine(this, wxID_STATIC);
 
