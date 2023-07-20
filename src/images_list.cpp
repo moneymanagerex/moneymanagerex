b35a254ff15ab14961fc8c7fbd1adb5db4a4f393
vomikan <nikolay.akimov@gmail.com>
2023-07-20 16:56:18 +0300

    fix(#6063): positional parameter required

--- src/images_list.cpp
+++ src/images_list.cpp
@@ -456,7 +456,7 @@ bool checkThemeContents(wxArrayString *filesinTheme)
         if (erroredIcons > maxCutOff) {
             missingIcons << " " << _("and more...");
         }
-        wxMessageBox(wxString::Format(_("There are %d missing or invalid icons in chosen theme '%1$s': %2$s")
+        wxMessageBox(wxString::Format(_("There are %1$d missing or invalid icons in chosen theme '%2$s': %3$s")
             , erroredIcons, Model_Setting::instance().Theme(), missingIcons), _("Warning"), wxOK | wxICON_WARNING);
     }
     return success;
