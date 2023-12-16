5ccf249ee59b41b30bca8137ae20163fb71a7e60
tasukus@gmail.com <tasukus@gmail.com>
2023-12-16 17:30:01 +0900

    fix used NULL at not pointer

--- src/transactionsupdatedialog.cpp
+++ src/transactionsupdatedialog.cpp
@@ -80,7 +80,7 @@ transactionsUpdateDialog::transactionsUpdateDialog(wxWindow* parent
             m_hasNonTransfers = true;
     }
 
-    m_custom_fields = new mmCustomDataTransaction(this, NULL, ID_CUSTOMFIELDS);
+    m_custom_fields = new mmCustomDataTransaction(this, 0, ID_CUSTOMFIELDS);
 
     this->SetFont(parent->GetFont());
     Create(parent);
