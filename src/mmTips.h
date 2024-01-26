b9bc86b5834e44ed9ddfb0cda733833289074238
ovari <17465872+ovari@users.noreply.github.com>
2024-01-26 21:50:37 +1100

    Update mmTips.h

--- src/mmTips.h
+++ src/mmTips.h
@@ -19,22 +19,19 @@ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
 
 static const wxString TIPS [] =
 {
-    wxTRANSLATE("Recommendation: Always backup your .mmb database file regularly."),
-    wxTRANSLATE("Recommendation: If upgrading to a new version of MMEX, make sure you backup your .mmb database file before doing so."),
-    wxTRANSLATE("Recommendation: Use copy (Ctrl+C) and paste (Ctrl+V) for frequently used transactions."),
-    wxTRANSLATE("Tip: Remember to make backups of your .mmb database file."),
-    wxTRANSLATE("Tip: The .mmb database file is not encrypted. That means anyone else having the proper know how can actually open the file and read the contents. So make sure that if you are storing any sensitive financial information it is properly guarded."),
-    wxTRANSLATE("Tip: To mark a transaction as reconciled, select the transaction and press the “R” key. To mark a transaction as unreconciled, select the transaction and press the “U” key."),
-    wxTRANSLATE("Tip: To mark a transaction as requiring followup, select the transaction and press the “F” key."),
-    wxTRANSLATE("Tip: MMEX supports printing of all reports that can be viewed. The print options are available under the menu, File → Print."),
-    wxTRANSLATE("Tip: You can modify some runtime behavior of MMEX by changing the settings in Options."),
-    wxTRANSLATE("Tip: To print a statement with transactions from any arbitrary set of criteria, use the transaction filter to select the transactions you want and then do a print from the menu."),
-    wxTRANSLATE("Tip: Set exchange rate for currencies in case if you have accounts with different currencies."),
-
-    wxTRANSLATE("Category Manager tip: Pressing the “H” key will cycle through all categories starting with the letter h."),
-    wxTRANSLATE("Category Manager tip: Pressing 2 key combination will cycle through all categories starting with that key combination. Example: Pressing ho will select Homeneeds, Home, House Tax, etc…"),
-
-    wxTRANSLATE("Never endorse a loan. If the borrower—your friend, family member, significant other, whoever—misses payments, your credit score will take a plunge, the lender can come after you for the money, and it will likely destroy your relationship. Plus, if the bank is requiring a cosigner, the bank does not trust the person to make the payments."),
+    wxTRANSLATE("MMEX tip: Always backup your .mmb database file regularly."),
+    wxTRANSLATE("MMEX tip: If upgrading to a new version of MMEX, make sure you backup your .mmb database file before doing so."),
+    wxTRANSLATE("MMEX tip: Use copy (Ctrl+C) and paste (Ctrl+V) for frequently used transactions."),
+    wxTRANSLATE("MMEX tip: Remember to make backups of your .mmb database file."),
+    wxTRANSLATE("MMEX tip: The .mmb database file is not encrypted. That means anyone else having the proper know how can actually open the file and read the contents. So make sure that if you are storing any sensitive financial information it is properly guarded."),
+    wxTRANSLATE("MMEX tip: To mark a transaction as reconciled, select the transaction and press the “R” key. To mark a transaction as unreconciled, select the transaction and press the “U” key."),
+    wxTRANSLATE("MMEX tip: To mark a transaction as requiring followup, select the transaction and press the “F” key."),
+    wxTRANSLATE("MMEX tip: MMEX supports printing of all reports that can be viewed. The print options are available under the menu, File → Print."),
+    wxTRANSLATE("MMEX tip: You can modify some runtime behavior of MMEX by changing the settings in Options."),
+    wxTRANSLATE("MMEX tip: To print a statement with transactions from any arbitrary set of criteria, use the transaction filter to select the transactions you want and then do a print from the menu."),
+    wxTRANSLATE("MMEX tip: Set exchange rate for currencies in case if you have accounts with different currencies."),
+    wxTRANSLATE("MMEX tip: Category Manager: Pressing the “H” key will cycle through all categories starting with the letter h."),
+    wxTRANSLATE("MMEX tip: Category Manager: Pressing 2 key combination will cycle through all categories starting with that key combination. Example: Pressing ho will select Homeneeds, Home, House Tax, etc…"),
 
     wxTRANSLATE("Grocery tip: Avoid grocery shopping when hungry or stressed. Hunger and stress can make you do strange things, like buying lots of chocolate or gimmicky processed foods. Plan to go to the store when you are well-fed and relaxed. That way, you can stick to your list and not overspend on food you do not need."),
     wxTRANSLATE("Grocery tip: Make a shopping list. A shopping list saves money by helping you avoid buying products you do not need. The simplest way is to track consumables as you use them. Keep a magnetic notepad on the fridge and jot down what you need as you run out."),
@@ -42,8 +39,6 @@ static const wxString TIPS [] =
     wxTRANSLATE("Grocery tip: Make sure your family consumes everything you purchase. Every month or so, hold a pantry challenge during which you use up as many pantry goods as possible before you shop again. A pantry challenge cuts down on waste and lets you get a sense of how much you are overbuying."),
     wxTRANSLATE("Grocery tip: Reduce waste. Adopting better grocery shopping habits is one way to cut back on wasted food and money. Make a list of everything you throw out without using, such as spoiled leftovers or soggy lettuce, to get a sense of the full scale of the issue."),
     wxTRANSLATE("Grocery tip: Read sales flyers. Review each store’s flyer and match what is on sale with what is on your list or plan meals around sale items.  It is sometimes more cost-effective to visit multiple stores in one week to get the best value."),
-
-    wxTRANSLATE("Cut back on subscriptions. Do you really need to subscribe to every streaming service? Are you using a paid service enough to justify the expense, or can you survive with the ad-supported free one? Do you need that monthly book club, or can you head to the library once per month instead?"),
 
     wxTRANSLATE("Software tip: Replace closed-source software with free/libre open-source software. Visit https://alternativeto.net/"),
     wxTRANSLATE("Software tip: Become productive with “LibreOffice Base” for databases. Visit https://www.libreoffice.org/discover/base/"),
@@ -73,7 +68,10 @@ static const wxString TIPS [] =
     wxTRANSLATE("Get out of debt tip: Spend less than you earn."),
     wxTRANSLATE("Get out of debt tip: Pay more than the minimum."),
     wxTRANSLATE("Get out of debt tip: If you have a mountain of debt, studies show paying off the smallest debts can give you the confidence to tackle the larger ones."),
-    wxTRANSLATE("Before going to a shop and buy something: take the time making a list of what you really need. In the shop buy what is on your list.")
+    wxTRANSLATE("Get out of debt tip: Never endorse a loan. If the borrower—your friend, family member, significant other, whoever—misses payments, your credit score will take a plunge, the lender can come after you for the money, and it will likely destroy your relationship. Plus, if the bank is requiring a cosigner, the bank does not trust the person to make the payments."),
+
+    wxTRANSLATE("Buying tip: Cut back on subscriptions. Do you really need to subscribe to every streaming service? Are you using a paid service enough to justify the expense, or can you survive with the ad-supported free one? Do you need that monthly book club, or can you head to the library once per month instead?"),
+    wxTRANSLATE("Buying tip: Before going to a shop to buy something. Take the time making a list of what you really need. In the shop buy only what is on your list.")
 };
 
 static const wxString STOCKTIPS[] = {
