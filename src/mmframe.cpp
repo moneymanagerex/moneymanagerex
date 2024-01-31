94c8cfc9cd596ce113d2ca798fea1f5c5a7b7f2f
ovari <17465872+ovari@users.noreply.github.com>
2024-01-31 11:45:58 +1100

    Update mmframe.cpp

--- src/mmframe.cpp
+++ src/mmframe.cpp
@@ -2245,7 +2245,7 @@ void mmGUIFrame::OnNew(wxCommandEvent& /*event*/)
         _("Choose database file to create"),
         wxEmptyString,
         wxEmptyString,
-        _("MMEX Database")+" (*.mmb)|*.mmb"),
+        _("MMEX Database")+" (*.mmb)|*.mmb",
         wxFD_SAVE | wxFD_OVERWRITE_PROMPT
     );
 
