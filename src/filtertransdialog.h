9f8d9a5ff4a79561812744180ea5982b104ee787
ovari <17465872+ovari@users.noreply.github.com>
2024-04-30 19:23:03 +1000

    Update filtertransdialog.h

--- src/filtertransdialog.h
+++ src/filtertransdialog.h
@@ -160,7 +160,7 @@ private:
     /// Creation
     bool Create(wxWindow* parent
         , wxWindowID id = wxID_ANY
-        , const wxString& caption = _("Transaction Report Filter")
+        , const wxString& caption = _("Customize Transaction Report")
         , const wxPoint& pos = wxDefaultPosition
         , const wxSize& size = wxDefaultSize
         , long style = wxCAPTION | wxSYSTEM_MENU | wxCLOSE_BOX
