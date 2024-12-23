7433235b326b2ea5fa8ecc017451acb64b2520d0
ovari <17465872+ovari@users.noreply.github.com>
2024-12-24 07:55:52 +1100

    Update qif_export.cpp

--- src/import_export/qif_export.cpp
+++ src/import_export/qif_export.cpp
@@ -200,7 +200,7 @@ void mmQIFExportDialog::CreateControls()
         , wxDefaultPosition, wxDefaultSize, wxCHK_2STATE );
     toFileCheckBox_->SetValue(true);
     file_name_label_ = new wxStaticText(main_tab, wxID_ANY, _("File Name:"));
-    button_search_ = new wxButton(main_tab, wxID_SAVE, _("&Browse..."));
+    button_search_ = new wxButton(main_tab, wxID_SAVE, _("&Browse…"));
     button_search_->Connect(wxID_SAVE, wxEVT_COMMAND_BUTTON_CLICKED
         , wxCommandEventHandler(mmQIFExportDialog::OnFileSearch), nullptr, this);
 
@@ -302,7 +302,7 @@ void mmQIFExportDialog::OnAccountsButton(wxCommandEvent& WXUNUSED(event))
     }
     else if (selected_accounts_id_.size() > 1)
     {
-        bSelectedAccounts_->SetLabelText("...");
+        bSelectedAccounts_->SetLabelText("…");
         mmToolTip(bSelectedAccounts_, baloon);
     }
 
