bddd3b92cc12a6ce8e04fb3a6d523ff92a466ccc
ovari <17465872+ovari@users.noreply.github.com>
2025-02-09 12:29:29 +1100

    Update categdialog.cpp

--- src/categdialog.cpp
+++ src/categdialog.cpp
@@ -445,7 +445,7 @@ void mmCategDialog::OnEndDrag(wxTreeEvent& event)
     }
 
     wxString moveMessage = wxString::Format(
-        _("Are you sure you want to move\n\"%1$s\"\nto:\n\"%2$s\"?")
+        _u("Do you want to to move\n“%1$s”\nto:\n“%2$s”?")
         , Model_Category::full_name(m_dragSourceCATEGID)
         , categID != -1 ? Model_Category::full_name(categID) : _("Top level"));
     wxMessageDialog msgDlg(this, moveMessage, _("Confirm Move"),
@@ -500,7 +500,7 @@ void mmCategDialog::mmDoDeleteSelectedCategory()
 
     wxMessageDialog msgDlg(this, _("Deleted transactions exist which use this category or one of its descendants.")
             + "\n\n" + _("Deleting the category will also automatically purge the associated deleted transactions.")
-            + "\n\n" + _("Do you wish to continue ?")
+            + "\n\n" + _("Do you want to continue?")
         , _("Confirm Category Deletion"), wxYES_NO | wxNO_DEFAULT | wxICON_WARNING);
     if ((deletedTrans.empty() && splits.empty()) || msgDlg.ShowModal() == wxID_YES)
     {
@@ -759,7 +759,7 @@ void mmCategDialog::OnMenuSelected(wxCommandEvent& event)
     }
     case MENU_ITEM_CLEAR:
     {
-        wxMessageDialog msgDlg(this, _("Are you sure you want to unhide all categories")
+        wxMessageDialog msgDlg(this, _("Do you want to unhide all categories?")
                 , _("Unhide all categories")
                 , wxYES_NO | wxNO_DEFAULT | wxICON_EXCLAMATION);
         if (msgDlg.ShowModal() == wxID_YES)
