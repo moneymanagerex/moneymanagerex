15e40933a7762054e7f0126793217f42b9bb4882
Klaus Wich <kwich@awasna.de>
2026-03-24 11:46:34 +0100

    Compiler warnings removed

--- src/panel/AssetList.cpp
+++ src/panel/AssetList.cpp
@@ -19,8 +19,6 @@
  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  ********************************************************/
 
-#pragma once
-
 #include "base/defs.h"
 #include <wx/srchctrl.h>
 
