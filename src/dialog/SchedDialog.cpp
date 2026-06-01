fb6aecf1ae8d8f2ace22a590ec13203b59f6347d
Klaus Wich <kwich@awasna.de>
2026-06-01 16:20:39 +0200

    (#8364) Copy tags, category and color while creating a scheduled transaction from an existing one

--- src/dialog/SchedDialog.cpp
+++ src/dialog/SchedDialog.cpp
@@ -3,7 +3,7 @@
  Copyright (C) 2016 Stefano Giorgio
  Copyright (C) 2016 - 2022 Nikolay Akimov
  Copyright (C) 2021, 2022 Mark Whalley (mark@ipx.co.uk)
- Copyright (C) 2025 KLaus Wich
+ Copyright (C) 2025, 2026 Klaus Wich
 
  This program is free software; you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
@@ -750,7 +750,10 @@ void SchedDialog::setDialogParameters(int64 trx_id)
     m_sched_d.m_number          = trx_dx.m_number;
     m_sched_d.m_notes           = trx_dx.m_notes;
     // m_sched_d.m_followup_id  : default
-    // m_sched_d.m_color        : default
+    if (trx_n->m_color > -1) {
+        m_sched_d.m_color       = trx_n->m_color;
+        w_color_btn->SetColor(m_sched_d.m_color.GetValue());
+    }
     // m_sched_d.m_due_date     : already set by constructor
     // m_sched_d.m_repeat       : default
 
@@ -761,10 +764,19 @@ void SchedDialog::setDialogParameters(int64 trx_id)
         split_d.m_category_id = tp_d.m_category_id;
         split_d.m_amount      = tp_d.m_amount;
         split_d.m_notes       = tp_d.m_notes;
+        // copy split tags:
+        for (const TagLinkData& gl_d : TagLinkModel::instance().find_data_a(
+            TagLinkCol::WHERE_REFID(OP_EQ, tp_d.m_id))
+        ) {
+            split_d.m_tag_id_a.push_back(gl_d.m_tag_id);
+        }
         m_split_a.push_back(split_d);
     }
 
-    // TODO: copy tags
+    // copy tags
+    if (!trx_dx.TAGNAMES.IsEmpty()) {
+        w_tag_text->SetText(trx_dx.TAGNAMES);
+    }
     // TODO: copy custom fields
 
     w_account_text->SetValue(trx_dx.ACCOUNTNAME);
@@ -904,7 +916,7 @@ void SchedDialog::setCategoryLabel()
 {
     w_split_btn->UnsetToolTip();
     if (!m_split_a.empty()) {
-        w_cat_text->SetLabelText(_t("Split Transaction"));
+        w_cat_text->ChangeValue(_t("Split Transaction"));
         w_amount_text->SetValue(TrxSplitModel::instance().get_total(m_split_a));
         m_sched_d.m_category_id_n = -1;
     }
@@ -1101,6 +1113,7 @@ void SchedDialog::onPayee(wxCommandEvent& WXUNUSED(event))
     // used for payee.
     // If this is a Split Transaction, ignore displaying last category for payee
     if (m_split_a.empty() &&
+        m_sched_d.m_category_id_n == -1 &&
         ( PrefModel::instance().getTransCategoryNone() == PrefModel::LASTUSED ||
             PrefModel::instance().getTransCategoryNone() == PrefModel::DEFAULT
         ) &&
